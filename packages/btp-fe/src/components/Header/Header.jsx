24c570a582e5c1b3adaa9e62f1e77ab06f1d8ce7
dsng <27849964+geart891@users.noreply.github.com>
2021-05-14 14:19:51 +0700

    update modal

--- packages/btp-fe/src/components/Header/Header.jsx
+++ packages/btp-fe/src/components/Header/Header.jsx
@@ -3,20 +3,22 @@ import styled from 'styled-components';
 import { Layout, Avatar } from 'antd';
 import PropTypes from 'prop-types';
 import Nav from './Nav';
-import { colors } from '../Styles/Colors';
-// import { BellOutlined } from '@ant-design/icons';
+// import { colors } from '../Styles/Colors';
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 // import { LanguageSwitcher } from '../LanguageSwitcher';
 
 // import { media } from '../Styles/Media';
 import { Dropdown } from '../Dropdown';
 import defaultAvatar from '../../assets/images/profile-img.png';
+import MetaMask from '../../assets/images/metal-mask.svg';
+import ICONex from '../../assets/images/icon-ex.svg';
 
 const StyledHeader = styled(Layout.Header)`
+  font-family: Poppins;
   height: 80px;
   width: 100%;
   padding: 0 160px 0 40.5px;
-  /* color: ${colors.textColor}; */
-  color: #fff;
+  color: #eff1ed;
   background-color: #131217;
   border-bottom: 1px solid #353242;
   display: flex;
@@ -70,6 +72,104 @@ const StyledHeader = styled(Layout.Header)`
       flex-wrap: nowrap;
     }
   }
+  .connect-a-wallet-modal {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    text-align: center;
+    letter-spacing: 1px;
+    h4 {
+      color: #eff1ed;
+      position: relative;
+      width: 100%;
+      display: grid;
+      grid-template-columns: 20% 60% 20%;
+      justify-content: center;
+      padding: 0 32px;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 25px;
+      /* line-height: 36px; */
+      letter-spacing: 1px;
+      color: #eff1ed;
+      margin-top: 23px;
+      margin-bottom: 31px;
+      .card-title {
+        grid-column: 2;
+        text-align: center;
+      }
+      > button {
+        width: auto;
+        height: auto;
+        background-color: transparent;
+        grid-column: 3;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        font-size: 18px;
+        margin: 0;
+      }
+    }
+
+    button {
+      margin: auto 0 32px;
+      width: 416px;
+      height: 64px;
+      background: #5465ff;
+      border-radius: 4px;
+    }
+    .connect-a-wallet-card {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 480px;
+      height: 370px;
+      background: #1d1b22;
+      border-radius: 4px;
+      .wallet-selector {
+        margin: 0 0 8px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: nowrap;
+        width: 416px;
+        height: 72px;
+        padding: 0 28.5px;
+        background: transparent;
+        color: #eff1ed;
+        .wallet-title {
+          margin-right: auto;
+          margin-left: 13.3px;
+          color: #eff1ed;
+          letter-spacing: 0.75px;
+        }
+        span {
+          grid-column: 3;
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          font-size: 18px;
+          color: #5ef38c;
+        }
+        &:hover,
+        :focus {
+          background: #312f39;
+          border-radius: 4px;
+        }
+      }
+    }
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.05);
+    backdrop-filter: blur(10px);
+    z-index: 1;
+  }
   .connect-to-wallet-btn {
     height: 44px;
     padding: 12px 16px;
@@ -106,23 +206,70 @@ const defaultUser = {
   authorized: false,
   avatar: defaultAvatar,
 };
+const wallets = {
+  metamask: {
+    id: 'metamask',
+    title: 'MetaMask Wallet',
+    icon: MetaMask,
+  },
+  iconex: {
+    id: 'iconex',
+    title: 'ICONex Wallet',
+    icon: ICONex,
+  },
+};
+
+const WalletSelector = ({ type, active, onClick }) => {
+  return (
+    <button id={wallets[type].id} className="wallet-selector" autoFocus={active} onClick={onClick}>
+      <Avatar src={wallets[type].icon} size={30} />
+      <span className="wallet-title">{wallets[type].title}</span> {active && <CheckOutlined />}
+    </button>
+  );
+};
 
 const Header = ({ items, userStatus = defaultUser, wallet = defaultWallet }) => {
-  const [authorized, setAuthorized] = useState(false);
+  const [authorized] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedWallet, setSelectedWallet] = useState('metamask');
   const handleConnect = (e) => {
     e.preventDefault();
-    setAuthorized((prev) => !prev);
+    setShowModal(true);
   };
-  window.testAuth = setAuthorized;
+  const handleSelectWallet = (e) => {
+    e.preventDefault();
+    setSelectedWallet(e.target.id);
+  };
+
   return (
     <StyledHeader>
+      {showModal && (
+        <div className="connect-a-wallet-modal">
+          <div className="connect-a-wallet-card">
+            <h4>
+              <span className="card-title">Connect a wallet</span>
+              <button onClick={() => void setShowModal(false)}>
+                <CloseOutlined />
+              </button>
+            </h4>
+            <WalletSelector
+              type="metamask"
+              active={selectedWallet == 'metamask'}
+              onClick={handleSelectWallet}
+            />
+            <WalletSelector
+              type="iconex"
+              active={selectedWallet == 'iconex'}
+              onClick={handleSelectWallet}
+            />
+            <button onClick={() => void console.log('wallet click')}>Connect a Wallet</button>
+          </div>
+        </div>
+      )}
       <div className="left-side">BTP Dashboard</div>
       <Nav />
       {userStatus.authorized || authorized ? (
         <div className="right-side">
-          {/* <span className="">
-            <BellOutlined />
-          </span> */}
           <span className="wallet-name">{wallet.name}</span>
           <Dropdown items={items} fullWidthOnMobile handleLogout={handleConnect}>
             <div className="dropdown-hoverable">
