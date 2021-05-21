de6366bbbe5c2959fe8b09ca33678a57ff750c20
Duy Phan <phanduy344@gmail.com>
2021-05-21 11:24:22 +0700

    feat/#16 approval transfer (#13)

--- packages/btp-fe/src/components/TransferBox/TransferBox.jsx
+++ packages/btp-fe/src/components/TransferBox/TransferBox.jsx
@@ -1,6 +1,8 @@
+import { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components/macro';
 
 import { Details } from './Details';
+import { Approval } from './Approval';
 import { colors } from '../Styles/Colors';
 
 const Wrapper = styled.div`
@@ -11,9 +13,28 @@ const Wrapper = styled.div`
 `;
 
 export const TransferBox = () => {
-  return (
-    <Wrapper>
-      <Details />
-    </Wrapper>
-  );
+  const [step, setStep] = useState(1);
+  const [wasBack, setWasBack] = useState(false);
+  const [tokenValue, setTokenValue] = useState('');
+
+  useEffect(() => {
+    setWasBack(true);
+  }, [step]);
+
+  const memoizedSetStep = useCallback((param) => setStep(param), [setStep]);
+  const memoizedSetTokenValue = useCallback((param) => setTokenValue(param), [setTokenValue]);
+
+  const steps = {
+    1: (
+      <Details
+        setStep={memoizedSetStep}
+        tokenValue={tokenValue}
+        setTokenValue={memoizedSetTokenValue}
+        initalInputDisplay={!wasBack}
+      />
+    ),
+    2: <Approval setStep={memoizedSetStep} tokenValue={tokenValue} />,
+  };
+
+  return <Wrapper>{steps[step]}</Wrapper>;
 };
