1d3b87492dc487587998eb580dbc80b5b12af129
Gia Luan Vuong <luanvuonggia@gmail.com>
2021-06-03 09:46:20 +0700

    feat: :sparkles: governance page (#28)
    Co-authored-by: Luan Vuong <luanvuong@orgos.net>

--- packages/btp-fe/src/Routes.jsx
+++ packages/btp-fe/src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from 'containers/HomePage';
 import NetworkPage from 'containers/NetworkPage';
 import NotFoundPage from 'containers/NotFoundPage';
+import Governance from 'containers/Governance';
 import Transfer from 'containers/Transfer';
 
 function Routes() {
@@ -12,6 +13,8 @@ function Routes() {
         <Route path="/" exact component={HomePage} />
         <Route path="/transfer" exact component={Transfer} />
         <Route path="/overview" exact component={Transfer} />
+        <Route path="/network" exact component={Transfer} />
+        <Route path="/governance" exact component={Governance} />
         <Route path="/network" exact component={NetworkPage} />
         <Route path="/governance" exact component={Transfer} />
         <Route path="/auction" exact component={Transfer} />
