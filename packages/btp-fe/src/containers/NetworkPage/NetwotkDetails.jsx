ce54da22d4368e69dd2b7630221c4537a6143f22
Duy Phan <duyphan@lecle.co.kr>
2021-06-03 14:31:02 +0700

    feat: created text with icon

--- packages/btp-fe/src/containers/NetworkPage/NetwotkDetails.jsx
+++ packages/btp-fe/src/containers/NetworkPage/NetwotkDetails.jsx
@@ -1,18 +1,27 @@
 import styled from 'styled-components';
+
+import { TextWithIcon } from 'components/TextWithIcon';
 import { Text } from 'components/Typography';
 import { colors } from 'components/Styles/Colors';
+import { media } from 'components/Styles/Media';
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 16px;
+  margin-top: 13px;
 
   max-height: 70vh;
   overflow-x: auto;
+
+  ${media.md`
+    grid-template-columns: 1fr;
+    max-height: 100vh;
+  `};
 `;
 
 const NetworkWrapper = styled.div`
-  padding: 16px 32px;
+  padding: 16px 32px 20px;
   border-radius: 4px;
   background-color: ${colors.brandSecondaryBase};
 
@@ -27,6 +36,10 @@ const NetworkWrapper = styled.div`
     }
   }
 
+  .all-time {
+    margin-top: 19px;
+  }
+
   .devider {
     border-bottom: solid 1px ${colors.grayLine};
     margin: 12px 0;
@@ -36,6 +49,9 @@ const NetworkWrapper = styled.div`
 const NetWork = () => {
   return (
     <NetworkWrapper>
+      <TextWithIcon icon="btc" width="24px">
+        Bitcoin BTC
+      </TextWithIcon>
       <div className="all-time">
         <Text className="small">All time</Text>
         <div className="values">
