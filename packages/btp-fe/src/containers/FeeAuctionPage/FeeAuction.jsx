e2c6fc427105e5461de06952a4a0ac59b4304baf
Duy Phan <duyphan@lecle.co.kr>
2021-06-07 10:00:13 +0700

    feat: search form

--- packages/btp-fe/src/containers/FeeAuctionPage/FeeAuction.jsx
+++ packages/btp-fe/src/containers/FeeAuctionPage/FeeAuction.jsx
@@ -1,21 +1,25 @@
 import styled from 'styled-components/macro';
 
 import { Header } from 'components/Typography';
+import { SearchForm } from './SearchForm';
 
 const Wrapper = styled.div`
   max-width: 1120px;
   margin: auto;
   padding: 52px 0 31px;
 
-  .search-control {
+  .search-group {
+    display: flex;
+    justify-content: space-between;
   }
 `;
 
 const FeeAuction = () => {
   return (
     <Wrapper>
-      <div className="search-control">
+      <div className="search-group">
         <Header className="medium bold">Fee auction</Header>
+        <SearchForm />
       </div>
     </Wrapper>
   );
---
+++ packages/btp-fe/src/containers/FeeAuctionPage/SearchForm.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components/macro';
+import { OvalTextInput } from 'components/Input/OvalTextInput';
+import { PrimaryButton } from 'components/Button';
+
+const Wrapper = styled.form`
+  display: flex;
+
+  & > button {
+    height: 100%;
+    margin-left: 16px;
+  }
+`;
+
+export const SearchForm = () => {
+  return (
+    <Wrapper>
+      <OvalTextInput />
+      <PrimaryButton type="submit">Search</PrimaryButton>
+    </Wrapper>
+  );
+};
