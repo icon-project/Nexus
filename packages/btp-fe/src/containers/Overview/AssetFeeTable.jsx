326db77b554c4a724d864ea10f9349fc7cabe429
Duy Phan <phanduy344@gmail.com>
2021-06-08 18:32:00 +0700

    feat: #62 fee auction overview (#38)
    * feat: mvp layout
    
    * feat: search form
    
    * refactor: created text with info
    
    * refactor: table
    
    * feat: table width
    
    * feat: define col width
    
    * feat: mobile view
    
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

--- packages/btp-fe/src/containers/Overview/AssetFeeTable.jsx
+++ packages/btp-fe/src/containers/Overview/AssetFeeTable.jsx
@@ -1,63 +1,25 @@
-import { useRef } from 'react';
 import styled from 'styled-components/macro';
 
 import { Icon } from 'components/Icon';
+import { HorizontalScrollContainer } from 'components/HorizontalScrollContainer';
 import { smallText } from 'components/Typography/Text';
 import { colors } from 'components/Styles/Colors';
 
-import leftArrow from '../../assets/images/left-arrow.svg';
+const Table = styled.table`
+  th,
+  td {
+    text-align: center;
+    padding: 14px 20px;
+    ${smallText};
 
-const Wrapper = styled.div`
-  width: 100%;
-  position: relative;
-
-  .container {
-    overflow-y: scroll;
-    width: 100%;
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
-
-    .content {
-      white-space: nowrap;
-
-      table {
-        th,
-        td {
-          text-align: center;
-          padding: 14px 20px;
-          ${smallText};
-
-          img {
-            margin-right: 4px;
-          }
-        }
-
-        tbody {
-          border-top: solid 1px ${colors.grayLine};
-          border-bottom: solid 1px ${colors.grayLine};
-        }
-      }
+    img {
+      margin-right: 4px;
     }
   }
 
-  button {
-    background: transparent center / 40% no-repeat url('${leftArrow}');
-    width: 15px;
-    height: 15px;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    left: -20px;
-
-    &.next {
-      transform: translateY(-50%) rotate(180deg);
-      left: unset;
-      right: -20px;
-    }
+  tbody {
+    border-top: solid 1px ${colors.grayLine};
+    border-bottom: solid 1px ${colors.grayLine};
   }
 `;
 
@@ -80,61 +42,28 @@ const values = [
 ];
 
 export const AssetFeeTable = () => {
-  const containerRef = useRef();
-
-  const sideScroll = (direction, speed, distance, step) => {
-    const element = containerRef.current;
-    let scrollAmount = 0;
-
-    var slideTimer = setInterval(function () {
-      if (direction == 'left') {
-        element.scrollLeft -= step;
-      } else {
-        element.scrollLeft += step;
-      }
-      scrollAmount += step;
-      if (scrollAmount >= distance) {
-        window.clearInterval(slideTimer);
-      }
-    }, speed);
-  };
-
-  const onNext = () => {
-    sideScroll('right', 25, 100, 10);
-  };
-
-  const onBack = () => {
-    sideScroll('left', 25, 100, 10);
-  };
-
   return (
-    <Wrapper>
-      <div className="container" ref={containerRef}>
-        <div className="content">
-          <table>
-            <thead>
-              <tr>
-                <th></th>
-                {values.map(({ icon, symbol }, idx) => (
-                  <TH icon={icon} key={idx}>
-                    {symbol}
-                  </TH>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Fee</td>
-                {values.map(({ fee }, idx) => (
-                  <td key={idx}>{fee}</td>
-                ))}
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <button onClick={onBack} type="button"></button>
-      <button onClick={onNext} className="next" type="button"></button>
-    </Wrapper>
+    <HorizontalScrollContainer>
+      <Table>
+        <thead>
+          <tr>
+            <th></th>
+            {values.map(({ icon, symbol }, idx) => (
+              <TH icon={icon} key={idx}>
+                {symbol}
+              </TH>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Fee</td>
+            {values.map(({ fee }, idx) => (
+              <td key={idx}>{fee}</td>
+            ))}
+          </tr>
+        </tbody>
+      </Table>
+    </HorizontalScrollContainer>
   );
 };
