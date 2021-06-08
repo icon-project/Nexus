6e1060407ea4facc4c6737bf543deb322ecd8348
Duy Phan <duyphan@lecle.co.kr>
2021-06-09 06:58:21 +0700

    fix: wrong max values

--- packages/btp-fe/src/utils/inputValidation.js
+++ packages/btp-fe/src/utils/inputValidation.js
@@ -2,4 +2,4 @@ export const composeValidators = (...validators) => (value) =>
   validators.reduce((error, validator) => error || validator(value), undefined);
 
 export const maxValue = (max, msg) => (value) =>
-  isNaN(value) || +value <= +max ? undefined : msg || `Should be greater than ${max}`;
+  isNaN(value) || +value <= +max ? undefined : msg || `Should be less than ${max}`;
