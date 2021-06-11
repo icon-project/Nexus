61e08ac8d7adb43d981a79e4daa41bfb060f2183
Duy Phan <phanduy344@gmail.com>
2021-06-11 17:06:58 +0700

    feat: #67 handle when metamask has not been installed (#48)
    * feat: add validation when get ethereum instance
    
    * feat: check if metamask has been installed
    
    * feat: check if iconex wallet has been installed
    
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

--- packages/btp-fe/src/Routes.jsx
+++ packages/btp-fe/src/Routes.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Layout } from 'components/Layout';
 
-import HomePage from 'containers/HomePage';
 import NetworkPage from 'containers/NetworkPage';
 import NotFoundPage from 'containers/NotFoundPage';
 import Governance from 'containers/Governance';
@@ -15,7 +14,7 @@ function Routes() {
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route path="/" exact component={HomePage} />
+          <Redirect from="/" to="/transfer" exact />
           <Route path="/transfer" exact component={Transfer} />
           <Route path="/overview" exact component={Overview} />
           <Route path="/network" exact component={NetworkPage} />
