79fad373d3c9ecb81b6e6bd6fd2cabad49cfd82e
Dang Minh Nhut <dangminhnhut@nhutdang.local>
2021-06-14 16:48:28 +0700

    Feat/#33: Update mint/burn fees

--- packages/dashboard-api/src/modules/btpnetwork/controller.js
+++ packages/dashboard-api/src/modules/btpnetwork/controller.js
@@ -8,19 +8,19 @@ const model = require('./model');
 async function getNetworkInfo(request, response) {
   const assets = await model.getAmountFeeAggregationSCORE();
   const totalNetworks = await model.getTotalNetworks();
-  //const totalTransactionAmount = await model.getTotalTransactionAmount();
-  //const totalTransaction = await model.getTotalTransaction();
+  const totalTransactionAmount = await model.getTotalTransactionAmount();
+  const totalTransaction = await model.getTotalTransaction();
   const networkConnectedIcon = await model.getListNetworkConnectedIcon();
   response.status(HttpStatus.OK).json({
     content: {
-      //volume: totalTransactionAmount,
+      volume: totalTransactionAmount,
       fee: {
         cumulativeAmount: 100000, // TODO: total tokens ever had in FeeAggregationSCORE
         currentAmount: 500, // TODO: total amount of tokens valid in FeeAggregationSCORE
         assets,
       },
       totalNetworks,
-      //totalTransaction,
+      totalTransaction,
       networkConnectedIcon
     },
   });
