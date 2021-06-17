5a86992e94fc16cbf393221d65454a9af35cf35d
Gia Luan Vuong <luanvuonggia@gmail.com>
2021-06-17 16:44:08 +0700

    feat/tooltip (#54)
    * feat::sparkles:tooltip component
    
    * tooltip for area chart and donut chart
    
    Co-authored-by: Luan Vuong <luanvuong@orgos.net>

--- packages/btp-fe/src/components/TextWithInfo/index.js
+++ packages/btp-fe/src/components/TextWithInfo/index.js
@@ -1,18 +1,51 @@
 import styled from 'styled-components/macro';
 import { SubTitle } from 'components/Typography';
-
+import { Tooltip } from 'components/Tooltip';
 import { colors } from 'components/Styles/Colors';
 import infoIcon from '../../assets/images/info-icon.svg';
 
 const StyledHeading = styled(SubTitle)`
   color: ${colors.grayScaleSubText};
+  display: inline-flex;
   margin-bottom: 10px;
+  img {
+    padding-left: 8.67px;
+  }
+  .icon-with-tooltip {
+    display: flex;
+    align-items: center;
+  }
+  .icon-with-tooltip:hover .left {
+    display: initial;
+  }
+  .left {
+    display: none;
+    margin-left: 38.42px;
+  }
+  .right {
+    display: none;
+  }
 `;
 
-export const TextWithInfo = ({ children, hasInfo = true, className }) => {
+export const TextWithInfo = ({
+  children,
+  hasInfo = true,
+  className,
+  tooltip,
+  width,
+  direction = 'left',
+}) => {
   return (
     <StyledHeading className={`small bold ${className}`}>
-      {children} {hasInfo && <img src={infoIcon} alt="icon" />}
+      {children}
+      {hasInfo && (
+        <div className="icon-with-tooltip">
+          <img src={infoIcon} alt="icon" />
+          <Tooltip width={width} direction={direction}>
+            {tooltip}
+          </Tooltip>
+        </div>
+      )}
     </StyledHeading>
   );
 };
---
+++ packages/btp-fe/src/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,83 @@
+import { colors } from 'components/Styles/Colors';
+import styled from 'styled-components/macro';
+import { smallText } from 'components/Typography/Text';
+
+const Wrapper = styled.div`
+  position: absolute;
+  padding: 4px 8px;
+  background-color: #1d1b22;
+  border-radius: 4px;
+  border: 1px solid #312f39;
+  width: ${({ width }) => `${width}px`};
+  z-index: 101;
+  div {
+    ${smallText}
+    word-break: break-word;
+    color: ${colors.grayText};
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  &:before {
+    content: '';
+    display: inline-block;
+    position: absolute;
+  }
+
+  &.left {
+    &:before {
+      border-style: solid;
+      left: 1px;
+      top: ${({ arrowPosition }) => arrowPosition};
+      border-width: 4.33px 7.5px 4.33px 0;
+      transform: translate(-100%, -50%);
+      border-color: transparent #1d1b22;
+    }
+  }
+
+  &.right {
+    &:before {
+      border-style: solid;
+      right: 1px;
+      top: ${({ arrowPosition }) => arrowPosition};
+      border-width: 4.33px 0 4.33px 7.5px;
+      transform: translate(100%, -50%);
+      border-color: transparent transparent transparent #1d1b22;
+    }
+  }
+  &.top {
+    &:before {
+      border-style: solid;
+      left: ${({ arrowPosition }) => arrowPosition};
+      top: 1px;
+      border-width: 0px 4.33px 7.5px 4.33px;
+      border-color: transparent transparent #1d1b22;
+      transform: translate(-50%, -100%);
+    }
+  }
+
+  &.bottom {
+    &:before {
+      border-style: solid;
+      left: ${({ arrowPosition }) => arrowPosition};
+      bottom: 1px;
+      border-width: 7.5px 4.33px 0 4.33px;
+      border-color: #1d1b22 transparent transparent;
+      transform: translate(-50%, 100%);
+    }
+  }
+`;
+
+export const Tooltip = ({ arrowPosition, direction, width, children }) => {
+  return (
+    <Wrapper className={direction} arrowPosition={arrowPosition} width={width}>
+      <div>{children || 'Default tooltip message'}</div>
+    </Wrapper>
+  );
+};
+
+Tooltip.defaultProps = {
+  direction: 'none',
+  arrowPosition: '50%',
+  width: 204,
+};
