2deeaec77cbf6cb9c13e0c1d75dc85de6a3ce979
Sang Lai <Corgi@Sangs-MacBook-Pro.local>
2021-06-17 14:15:10 +0700

    refactor(transactions): move logic of transaction out the block indexer

--- packages/block-indexer/src/modules/icon-indexer/block-indexer.js
+++ packages/block-indexer/src/modules/icon-indexer/block-indexer.js
@@ -3,10 +3,10 @@
 const debug = require('debug')('icon');
 const IconService = require('icon-sdk-js');
 const { HttpProvider } = require('icon-sdk-js');
-const { logger, RESULT_CODE } = require('../../common');
-const { hexToDecimal } = require('../../common/util');
-const { saveBlock, saveTransaction, setTransactionConfirmed, getBySerialNumber, getLastBlock } = require('./repository');
+const { logger } = require('../../common');
+const { saveBlock, getLastBlock } = require('./repository');
 const { handleAuctionEvents } = require('./auctions');
+const { handleTransEvent } = require('./transactions');
 
 const httpProvider = new HttpProvider(process.env.ICON_API_URL);
 const iconService = new IconService(httpProvider);
@@ -14,53 +14,6 @@ const iconService = new IconService(httpProvider);
 let blockHeight = Number(process.env.ICON_BLOCK_HEIGHT);
 let isWaitToStop = false;
 
-/**
- * Confirm TransferEnd event
- * @param {*} event
- * @param {*} txInfo
- */
-async function confirmTransfEnd(event, txInfo) {
-  let indexedData = event.indexed;
-  let data = event.data;
-  let transaction = await getBySerialNumber(hexToDecimal(indexedData[1]));
-  // has transaction and transfer result code must be 0
-  if (transaction && hexToDecimal(data[0]) == RESULT_CODE.RC_OK) {
-    await setTransactionConfirmed([transaction], txInfo)
-  }
-}
-
-/**
- * Handle TransferStart and TransferEnd events
- * @param {*} txResult
- */
-async function handleTransEvent(txResult) {
-  let transactions = [];
-
-  for (let event of txResult.eventLogs) {
-    //handle TransactionStart event
-    if (event.indexed.find(item => item.includes('TransferStart'))) {
-      let indexedData = event.indexed;
-      let data = event.data;
-      let transObj = {
-        fromAddress: indexedData[1],
-        tokenName: indexedData[2],
-        serialNumber: hexToDecimal(data[0]),
-        value: hexToDecimal(data[1]),
-        toAddress: data[2],
-        txHash: txResult.txHash,
-        blockHash: txResult.blockHash,
-        blockHeight: txResult.blockHeight,
-        confirmed: false,
-      }
-      transactions.push(transObj)
-    } else if ((event.indexed.find(item => item.includes('TransferEnd')))) {
-      confirmTransfEnd(event, { txHash: txResult.txHash, blockHeight: txResult.blockHeight, blockHash: txResult.blockHash })
-    }
-  }
-  if (transactions.length > 0) {
-    await saveTransaction(transactions);
-  }
-}
 
 async function runTransactionHandlers(transaction, txResult, block) {
   await handleTransEvent(txResult);
--- packages/block-indexer/src/modules/icon-indexer/repository.js
+++ packages/block-indexer/src/modules/icon-indexer/repository.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const debug = require('debug')('db');
-const { v4: uuidv4 } = require('uuid');
-const { TRANSACTION_TBL_NAME, TRANSACTION_TBL, pgPool, logger } = require("../../common");
-const { sortValuesWithPropsOrdered, propsAsString, propsCountValueString, getCurrentTimestamp } = require("../../common/util");
+const { pgPool, logger } = require("../../common");
 
 async function saveBlock(block) {
   try {
@@ -29,81 +27,9 @@ async function getLastBlock() {
   }
 }
 
-/**
- * Pre-save transaction
- * @param {*} transfer
- */
-function preSave(transfer) {
-  if (!transfer.id) {
-    transfer.id = uuidv4();
-    transfer.createAt = getCurrentTimestamp();
-  }
-  transfer.updateAt = getCurrentTimestamp();
-  transfer.deleteAt = 0;
-}
 
-/**
- * Batch save transaction
- * @param {*} transactions
- */
-async function saveTransaction(transactions) {
-  try {
-    const client = await pgPool.connect();
-    await client.query('BEGIN');
-    for (let trans of transactions) {
-      preSave(trans);
-      const insertDealer = `INSERT INTO ${TRANSACTION_TBL_NAME} (${propsAsString(TRANSACTION_TBL)}) VALUES (${propsCountValueString(TRANSACTION_TBL)})`;
-      const insertDealerValues = sortValuesWithPropsOrdered(trans, TRANSACTION_TBL);
-      await client.query(insertDealer, insertDealerValues);
-      logger.info('SQL statement insert Transaction %0:', insertDealer, insertDealerValues);
-    }
-    await client.query('COMMIT');
-  } catch (error) {
-    logger.error(`saveTransferStart Failed save transaction result: ${trans.txHash} ${trans.blockHeight}`, { error });
-    throw error;
-  }
-}
-
-/**
- * Update confirmed status and block info when the transactions has event TransferEnd
- * @param {*} transactions
- * @param {*} txInfo
- */
-async function setTransactionConfirmed(transactions, txInfo) {
-  try {
-    const client = await pgPool.connect();
-    await client.query('BEGIN');
-    for (let transt of transactions) {
-      preSave(transt);
-      await client.query(`
-      UPDATE ${TRANSACTION_TBL_NAME}
-        SET
-          ${TRANSACTION_TBL.confirmed} = true,
-          ${TRANSACTION_TBL.blockHash} = $1,
-          ${TRANSACTION_TBL.blockHeight} = $2,
-          ${TRANSACTION_TBL.txHash} = $3
-        WHERE ${TRANSACTION_TBL.id} = $4`, [txInfo.blockHash, txInfo.blockHeight, txInfo.txHash, transt.id]);
-    }
-    await client.query('COMMIT');
-  } catch (error) {
-    logger.error(`saveTransferStart Failed to set confirmed for transaction result: ${txInfo.txHash},  ${txInfo.blockHeight}`, { error });
-    throw error;
-  }
-}
-
-/**
- * Get transaction by serial number
- * @param {*} serialNumber
- */
-async function getBySerialNumber(serialNumber) {
-  let { rows } = await pgPool.query(`SELECT * FROM  ${TRANSACTION_TBL_NAME} WHERE serial_number = $1`, [serialNumber]);
-  return rows[0];
-}
 
 module.exports = {
   saveBlock,
-  saveTransaction,
-  getBySerialNumber,
-  setTransactionConfirmed,
   getLastBlock
 };
