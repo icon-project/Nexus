b239ef640bd4bbd2e9130e2dda95cb7dd03ac601
Dang Minh Nhut <dangminhnhut@nhutdang.local>
2021-06-21 11:44:07 +0700

    Feat/#33 update mint_fee/burn_fee for networks

--- packages/dashboard-api/scripts/db/networks.sql
+++ packages/dashboard-api/scripts/db/networks.sql
@@ -2,7 +2,9 @@ CREATE TABLE public.networks (
     name character varying(4000) NOT NULL,
     id character varying NOT NULL,
     path character varying(100),
-    url character varying(100)
+    url character varying(100),
+    mint_fee numeric,
+    burn_fee numeric
 );
 
 
