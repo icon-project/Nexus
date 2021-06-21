7df5dcea87abd8ffe156d06766c9de0c64b6d329
Duy Phan <phanduy344@gmail.com>
2021-06-21 12:05:57 +0700

    feat: #91 handle errors (#76)
    * feat: (#91) handle errors
    
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

--- packages/btp-fe/src/store/models/app.js
+++ packages/btp-fe/src/store/models/app.js
@@ -12,14 +12,14 @@ const app = {
       };
     },
   },
-  effects: () => ({
+  effects: (dispatch) => ({
     async getAppInfo() {
       try {
         const appInfo = await fetchAPI('/btpnetwork');
         this.setAppInfo(appInfo || {});
         return appInfo;
       } catch (error) {
-        console.log(error);
+        dispatch.modal.handleError();
       }
     },
   }),
