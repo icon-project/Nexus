0f139985422045beb2ddd29aa3bc1e27849de7c6
Gia Luan Vuong <luanvuonggia@gmail.com>
2021-06-24 13:59:00 +0700

    fix: #75 mobile view for donut chart (#93)
    * fix/#75 mobile view for donut chart
    
    * refactor: center chart
    
    * chore: remove space
    
    Co-authored-by: Luan Vuong <luanvuong@orgos.net>
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

--- packages/btp-fe/src/components/DonutChart/DonutChart.jsx
+++ packages/btp-fe/src/components/DonutChart/DonutChart.jsx
@@ -6,6 +6,7 @@ import { Tooltip } from 'components/Tooltip';
 
 import { colors } from 'components/Styles/Colors';
 import { smallText } from 'components/Typography/Text';
+import { media } from 'components/Styles/Media';
 
 const Wrapper = styled.div`
   ${smallText}
@@ -46,6 +47,21 @@ const Wrapper = styled.div`
       color: ${colors.graySubText};
     }
   }
+  ${media.sm`
+    flex-direction: column;
+
+    .pie-chart {
+      align-self: center;
+      margin-right: 0;
+      margin-bottom: 30px;
+      width: 180px;
+      height: 180px;
+    }
+    .desc-element {
+      margin-bottom: 10px;
+      height: 12px;
+    }
+  `};
 `;
 
 const Circle = styled.div`
