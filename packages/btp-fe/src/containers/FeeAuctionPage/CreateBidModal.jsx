32f2ee131f32aaa9f60f31e886362a4c53774cd1
Duy Phan <phanduy344@gmail.com>
2021-06-28 09:21:28 +0700

    feat: #116 UI first bid (#105)
    * feat: added create bid button
    
    * feat: styled select input
    
    * feat: style buttons
    
    * feat: handle toggle modal
    
    * feat: update search form
    
    * chore: revert code
    
    * feat: update mobile view
    
    * fix: spaces
    
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

---
+++ packages/btp-fe/src/containers/FeeAuctionPage/CreateBidModal.jsx
@@ -0,0 +1,95 @@
+import { Form as FinalForm, Field } from 'react-final-form';
+import styled from 'styled-components/macro';
+
+import { Text } from 'components/Typography';
+import { Modal } from 'components/NotificationModal';
+import { TextInput } from 'components/Input';
+import { PrimaryButton, Button } from 'components/Button';
+import { SelectInput } from 'components/Select/SelectInput';
+import { colors } from 'components/Styles/Colors';
+
+import { minValue } from 'utils/inputValidation';
+
+const Form = styled.form`
+  text-align: left;
+  margin-top: 27px;
+
+  .input-field {
+    margin-bottom: 32px;
+
+    p.small {
+      margin-bottom: 6px;
+    }
+  }
+
+  .control-buttons {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const CreateBidModal = ({ setOpen }) => {
+  const onSubmit = (values) => {
+    console.log('values', values);
+  };
+
+  const assets = [
+    { value: 'bsc', label: 'BTC' },
+    { value: 'ed', label: 'ETH' },
+    { value: 'ic', label: 'SHIBA' },
+  ];
+
+  return (
+    <Modal title="New bid" display hasClosedBtn={false}>
+      <FinalForm
+        onSubmit={onSubmit}
+        render={({ handleSubmit, valid }) => {
+          return (
+            <Form onSubmit={handleSubmit}>
+              <div className="input-group">
+                <div className="input-field">
+                  <Text className="small">Asset type</Text>
+                  <SelectInput options={assets} />
+                </div>
+
+                <div className="input-field">
+                  <Text className="small">Bid amount</Text>
+                  <Field
+                    name="bidAmount"
+                    validate={minValue(100, 'Minimum bid is 100 ICX. Please input again')}
+                    render={({ input, meta }) => (
+                      <TextInput
+                        placeholder="Place a bid higher than 100 ICX"
+                        type="number"
+                        {...input}
+                        meta={meta}
+                      />
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="control-buttons">
+                <Button
+                  width={192}
+                  height={64}
+                  borderColor={colors.primaryBrandLight}
+                  backgroundColor="transparent"
+                  textColor={colors.primaryBrandLight}
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <PrimaryButton htmlType="submit" disabled={!valid} width={192} height={64}>
+                  Create new bid
+                </PrimaryButton>
+              </div>
+            </Form>
+          );
+        }}
+      />
+    </Modal>
+  );
+};
