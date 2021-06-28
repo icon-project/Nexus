73d5f158513cedf07b5bd121bb072be3b24e60f9
Nhi Nguyễn <nhinguyen@Nhis-MacBook-Air.local>
2021-06-28 09:50:13 +0700

    Feat/#31 update sql script to create table minted

---
+++ packages/dashboard-api/scripts/db/minted.sql
@@ -0,0 +1,14 @@
+-- Table: public.minted
+
+-- DROP TABLE public.minted;
+
+CREATE TABLE IF NOT EXISTS public.minted (
+    id character varying(100) PRIMARY KEY,
+    network_id character varying(100),
+    token_name character varying(100),
+    token_value numeric,
+    block_time bigint,
+    create_at integer,
+    update_at integer,
+    delete_at integer
+);
