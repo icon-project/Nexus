be7fe04e291550956a2b4b30228b806375987005
Tien Do <tiendq@gmail.com>
2021-06-30 11:35:21 +0700

    feat: add /auctions/:id/bids API for bid history (#96)

--- packages/dashboard-api/src/modules/auctions/route.js
+++ packages/dashboard-api/src/modules/auctions/route.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const debug = require('debug')('api:auctions');
 const { asyncMiddleware, debugLogMiddleware } = require('../../middlewares');
-const { getCurrentAuctions, getAuctionDetail, createNewAuction } = require('./controller');
+const { getCurrentAuctions, getAuctionDetail, createNewAuction, getBidHistory } = require('./controller');
 
 let router = express.Router();
 
 router.use(debugLogMiddleware(debug));
 router.get('/', asyncMiddleware(getCurrentAuctions));
 router.get('/:id', asyncMiddleware(getAuctionDetail));
+router.get('/:id/bids', asyncMiddleware(getBidHistory));
 router.post('/', asyncMiddleware(createNewAuction));
 
 module.exports = router;
