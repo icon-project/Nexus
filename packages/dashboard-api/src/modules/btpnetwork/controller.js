5a3ff115f4c8554cedf80ff721d0cb3fae263f99
Nhi Nguyễn <nhinguyen@nhutdang.local>
2021-07-01 16:12:14 +0700

    feat130/ recover param convert_to

--- packages/dashboard-api/src/modules/btpnetwork/controller.js
+++ packages/dashboard-api/src/modules/btpnetwork/controller.js
@@ -32,13 +32,13 @@ async function getNetworkInfo(request, response) {
 }
 
 async function getPriceConversion(request, response) {
-  if (!request.query.token || !request.query.amount || !request.query.tokens_convert_to) {
+  if (!request.query.token || !request.query.amount || !request.query.convert_to) {
     return response.sendStatus(HttpStatus.BadRequest);
   }
 
   const baseToken = request.query.token;
   const amount = parseInt(request.query.amount);
-  const tokensToConvertTo = request.query.tokens_convert_to.split(',');
+  const tokensToConvertTo = request.query.convert_to.split(',');
   
   const priceTokens = await model.getTokensPriceConversion(baseToken, amount, tokensToConvertTo);
   
