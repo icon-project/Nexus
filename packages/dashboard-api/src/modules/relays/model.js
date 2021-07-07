5dd488f9bb1c8f38fc5ec5474ade0588e383ea5c
sanglt1902 <38729663+sanglt1902@users.noreply.github.com>
2021-07-07 15:19:45 +0700

    refactor(relays): add monthly reward field to detail relay (#148)
    Co-authored-by: Sang Lai <Corgi@Sangs-MacBook-Pro.local>

--- packages/dashboard-api/src/modules/relays/model.js
+++ packages/dashboard-api/src/modules/relays/model.js
@@ -1,20 +1,23 @@
 'use strict';
 
-const { countTotalRelay, getRelayDetailList, getById } = require('./repository');
+const { countTotalRelay, getRelayDetailList } = require('./repository');
 
 async function getTotalRelay() {
   return await countTotalRelay();
 }
 
 async function getRelayList() {
-  return await getRelayDetailList();
+  let relays = await getRelayDetailList();
+
+  // TODO it should call to BMC to get total amount of monthly reward fund for Relay Candidate
+  // follow this issue to get final discussion https://git.baikal.io/btp-dashboard/pm/-/issues/37
+  // just the sameple data
+  relays.map(item => item.monthlyReward = Math.random());
+  return relays;
 }
 
-async function getRelayById(id) {
-  return getById(id);
-}
+
 module.exports = {
   getTotalRelay,
   getRelayList,
-  getRelayById,
 };
