efcd2154f83d0bbdbba36457081adb21eb820ffc
Nhi Nguyễn <nhinguyen@nhutdang.local>
2021-07-08 13:26:53 +0700

    update scripts for initial data of networks

--- packages/dashboard-api/scripts/db/tables.sql
+++ packages/dashboard-api/scripts/db/tables.sql
@@ -32,21 +32,4 @@ CREATE TABLE IF NOT EXISTS public.networks (
    delete_at integer
 );
 
-ALTER TABLE networks
-ALTER COLUMN id TYPE char varying(100);
-
 SELECT SUM(value) FROM Transactions WHERE Confirmed = true
-
--- Test data for public.relays
-
-INSERT INTO public.relays(
-	id, rank, name, bonded_icx, server_status, total_transferred_tx, total_failed_tx, created_time, updated_time)
-	VALUES ('2c37741ffa', 1, 'Relay 1', 1000, 1, 100, 0, NOW(), NOW());
-
-INSERT INTO public.relays(
-	id, rank, name, bonded_icx, server_status, total_transferred_tx, total_failed_tx, created_time, updated_time)
-	VALUES ('2c37741ffb', 2, 'Relay 2', 2000, 2, 1000, 10, NOW(), NOW());
-
-INSERT INTO public.relays(
-	id, rank, name, bonded_icx, server_status, total_transferred_tx, total_failed_tx, created_time, updated_time)
-	VALUES ('2c37741ffc', 3, 'Relay 3', 10450, 3, 101230, 210, NOW(), NOW());
