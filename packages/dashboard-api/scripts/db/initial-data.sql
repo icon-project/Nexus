3ad949b11142f275390d44a53803314da0a4e349
Nhi Nguyễn <nhinguyen@nhutdang.local>
2021-07-14 16:15:41 +0700

    update info network moonbean and timestamp

--- packages/dashboard-api/scripts/db/initial-data.sql
+++ packages/dashboard-api/scripts/db/initial-data.sql
@@ -1,27 +1,14 @@
--- Test data for public.networks
+-- Initial data for public.networks (Issue #33). 
+-- Note: run script in migration.sql to update table networks before run scrips bellow
 
-INSERT INTO networks(id, name, path_logo, url, mint_fee, burn_fee)
-values('0x1', 'Icon', './image/logo/icon-icx-logo.png', 'https://iconrepublic.org/', 0.01, 0.01);
+INSERT INTO networks(id, name, native_token, path_logo, url, mint_fee, burn_fee, create_at, update_at)
+values('0x1', 'Icon', 'ICX', './image/logo/icon-icx-logo.png', 'https://iconrepublic.org/', 0.01, 0.01, NOW(), NOW());
 
-INSERT INTO networks(id, name, path_logo, url, mint_fee, burn_fee)
-values('0x2', 'Edgeware', './image/logo/edgeware-edg-logo.png', 'https://edgewa.re/', 0.02, 0.01);
+INSERT INTO networks(id, name, native_token, path_logo, url, mint_fee, burn_fee, create_at, update_at)
+values('0x2', 'Moonbeam', 'GLMR','./image/logo/moonbeam-glmr-logo.png', 'https://moonbeam.network/', 0.02, 0.01, NOW(), NOW());
 
-INSERT INTO networks(id, name, path_logo, url, mint_fee, burn_fee)
-values('0x3', 'NEAR', './image/logo/near-protocol-near-logo.png', 'https://near.org/', 0.02, 0.02);
+INSERT INTO networks(id, name, native_token, path_logo, url, mint_fee, burn_fee, create_at, update_at)
+values('0x3', 'NEARProtocol', 'NEAR','./image/logo/near-protocol-near-logo.png', 'https://near.org/', 0.02, 0.02, NOW(), NOW());
 
-INSERT INTO networks(id, name, path_logo, url, mint_fee, burn_fee)
-values('0x4', 'Binance', './image/logo/binance-bnb-logo.png', 'https://www.binance.com/', 0.01, 0.02);
-
--- Test data for public.relays
-
-INSERT INTO public.relays(
-	id, rank, name, bonded_icx, server_status, total_transferred_tx, total_failed_tx, created_time, updated_time)
-	VALUES ('2c37741ffa', 1, 'Relay 1', 1000, 1, 100, 0, NOW(), NOW());
-
-INSERT INTO public.relays(
-	id, rank, name, bonded_icx, server_status, total_transferred_tx, total_failed_tx, created_time, updated_time)
-	VALUES ('2c37741ffb', 2, 'Relay 2', 2000, 2, 1000, 10, NOW(), NOW());
-
-INSERT INTO public.relays(
-	id, rank, name, bonded_icx, server_status, total_transferred_tx, total_failed_tx, created_time, updated_time)
-	VALUES ('2c37741ffc', 3, 'Relay 3', 10450, 3, 101230, 210, NOW(), NOW());
+INSERT INTO networks(id, name, native_token, path_logo, url, mint_fee, burn_fee, create_at, update_at)
+values('0x4', 'Binance', 'BNB','./image/logo/binance-bnb-logo.png', 'https://www.binance.com/', 0.01, 0.02, NOW(), NOW());
--- packages/dashboard-api/scripts/db/migration.sql
+++ packages/dashboard-api/scripts/db/migration.sql
@@ -16,3 +16,13 @@ WHERE transfer_fees.id=b.id
 
 ALTER TABLE transactions
   ADD total_volume numeric(100,6) NOT NULL DEFAULT 0,
+
+-- Issue #33
+
+ALTER TABLE networks
+DROP COLUMN delete_at,
+ADD COLUMN native_token character varying(100);
+
+ALTER TABLE networks
+ALTER COLUMN create_at SET DATA TYPE timestamp without time zone USING to_timestamp(create_at),
+ALTER COLUMN update_at SET DATA TYPE timestamp without time zone USING to_timestamp(update_at);
