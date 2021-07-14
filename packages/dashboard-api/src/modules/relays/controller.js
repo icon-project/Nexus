dad6d35a2f98ba5152c6b3e3679e9ca325b2d743
Tien Do <tiendq@gmail.com>
2021-07-14 14:39:08 +0700

    feat: get number of registered relay candidate change in last 24h (#170)

--- packages/dashboard-api/src/modules/relays/controller.js
+++ packages/dashboard-api/src/modules/relays/controller.js
@@ -5,23 +5,25 @@ const model = require('./model');
 
 // Show the list of registered relays.
 // GET /relays
+// curl http://localhost:8000/v1/relays | jq
 async function getRelayList(request, response) {
-  let style = request.query.style;
-
-  if (!style) style = 'list';
+  let style = request.query.style || 'list';
 
   switch (style) {
-  case 'count':
-    return await getTotalRelay(request, response);
+    case 'count':
+      return await getTotalRelay(request, response);
 
-  case 'reward':
-    return response.sendStatus(HttpStatus.NotImplemented);
+    case 'reward':
+      return response.sendStatus(HttpStatus.NotImplemented);
 
-  case 'list':
-    return await getRelayDetailList(request, response);
+    case 'list':
+      return await getRelayDetailList(request, response);
 
-  default:
-    return response.sendStatus(HttpStatus.BadRequest);
+    case 'registeredLast24h':
+      return await getRegisteredRelayChange(request, response);
+
+    default:
+      return response.sendStatus(HttpStatus.BadRequest);
   }
 }
 
@@ -45,8 +47,17 @@ async function getRelayDetailList(request, response) {
   });
 }
 
+// GET /relays?style=registeredLast24h
+async function getRegisteredRelayChange(request, response) {
+  const last24hChange = await model.getRegisteredChangeLast24h();
 
+  response.status(HttpStatus.OK).json({
+    content: {
+      last24hChange
+    }
+  });
+}
 
 module.exports = {
-  getRelayList,
+  getRelayList
 };
