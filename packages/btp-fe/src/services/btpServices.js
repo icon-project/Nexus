7dc2673eade3fabc5ebcb1c26984088dbe15d442
Gia Luan Vuong <luanvuonggia@gmail.com>
2021-07-22 11:32:20 +0700

    feat/#205-integrate-history-transaction-filtering (#197)
    * feat/#205-integrate-history-transaction-filtering
    
    * change limit to 20
    
    Co-authored-by: Luan Vuong <luanvuong@orgos.net>

--- packages/btp-fe/src/services/btpServices.js
+++ packages/btp-fe/src/services/btpServices.js
@@ -48,9 +48,10 @@ export const getNetwork = (id) => {
   return fetchAPI(`/networks/${id}`);
 };
 
-// transactions?page=<page>&limit=<limit>&from=<network_id>&to=<network_id> / default limit: 20
-export const getTransferHistory = (page, limit = 20) => {
-  return fetchAPI(`/transactions?page=${page}&limit=${limit}`);
+export const getTransferHistory = (page, limit = 20, assetName, from, to) => {
+  return fetchAPI(
+    `/transactions?page=${page}&limit=${limit}&assetName=${assetName}&from=${from}&to=${to}`,
+  );
 };
 
 export const getTransferHistoryById = (id) => {
