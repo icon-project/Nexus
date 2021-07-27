8c85e9ac698cd42a9814dc06fb7d18d739fa358e
Duy Phan <duyphan@lecle.co.kr>
2021-07-27 10:41:25 +0700

    refactor: network screen

--- packages/btp-fe/src/containers/Overview/Overview.jsx
+++ packages/btp-fe/src/containers/Overview/Overview.jsx
@@ -4,16 +4,11 @@ import styled from 'styled-components/macro';
 
 import { ValuesArea } from './ValuesArea';
 import { StatisticArea } from './StatisticArea';
-import { media } from 'components/Styles/Media';
 
 const Wrapper = styled.div`
   max-width: 1120px;
   margin: 0 auto;
   padding: 24px 0;
-
-  ${media.xl`
-    padding: 24px 12px;
-  `}
 `;
 
 const Overview = () => {
