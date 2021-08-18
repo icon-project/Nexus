7946a2dd8730c73f4f9478a861ee06c89694fbd2
Nhi Nguyễn <nhinguyen@nhutdang.local>
2021-08-18 16:13:13 +0700

    Feat/#222 get mint/burn value from MOONBEAM network

--- packages/block-indexer/src/modules/moonbeam-indexer/repository.js
+++ packages/block-indexer/src/modules/moonbeam-indexer/repository.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { logger, pgPool } = require('../../common');
+const { v4: uuidv4 } = require('uuid');
 
 async function saveBlock(block) {
   try {
@@ -29,8 +30,53 @@ async function getLastSavedBlock() {
 async function saveTransaction(transaction) {
 }
 
+async function saveMintEvent(mintObj, totalToken) {
+  preSave(mintObj);
+
+  const totalTokenAmount = totalToken + mintObj.tokenValue;
+  const query = 'INSERT INTO minted_tokens (id, network_id, token_name, token_value, total_token_amount, block_time, block_height, block_hash, tx_hash, mint_to, token_id, create_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, NOW())';
+  const values = [mintObj.id, process.env.MOONBEAM_NETWORK_ID, mintObj.tokenName, mintObj.tokenValue, totalTokenAmount, mintObj.blockTime, mintObj.blockHeight , mintObj.blockHash , mintObj.txHash, mintObj.to, mintObj.tokenId];
+
+  await pgPool.query(query, values);
+}
+
+async function saveBurnEvent(burnObj, totalToken) {
+  preSave(burnObj);
+
+  const totalTokenAmount = totalToken + burnObj.tokenValue;
+  const query = 'INSERT INTO burned_tokens (id, network_id, token_name, token_value, total_token_amount, block_time, block_height, block_hash, tx_hash, burn_from, token_id, create_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, NOW())';
+  const values = [burnObj.id, process.env.MOONBEAM_NETWORK_ID, burnObj.tokenName, burnObj.tokenValue, totalTokenAmount, burnObj.blockTime, burnObj.blockHeight , burnObj.blockHash , burnObj.txHash, burnObj.from, burnObj.tokenId];
+
+  await pgPool.query(query, values);
+}
+
+/**
+ * Pre-save mint/burn object
+ * @param {*} data
+ */
+function preSave(data) {
+  if (!data.id) {
+    data.id = uuidv4();
+    data.createAt = Math.floor(new Date().getTime());
+  }
+}
+
+async function getTotalTokenMinted(name) {
+  const { rows } = await pgPool.query('SELECT total_token_amount FROM minted_tokens WHERE token_name = $1 ORDER BY create_at DESC LIMIT 1', [name]);
+  return rows[0] ? Number(rows[0].total_token_amount) : 0;
+}
+
+async function getTotalTokenBurned(name) {
+  const { rows } = await pgPool.query('SELECT total_token_amount FROM burned_tokens WHERE token_name = $1 ORDER BY create_at DESC LIMIT 1', [name]);
+  return rows[0] ? Number(rows[0].total_token_amount) : 0;
+}
+
 module.exports = {
   saveBlock,
   saveTransaction,
-  getLastSavedBlock
+  getLastSavedBlock,
+  saveBurnEvent,
+  saveMintEvent,
+  getTotalTokenMinted,
+  getTotalTokenBurned
 };
