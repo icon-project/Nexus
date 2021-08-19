01d53eeff6071123e6b16e226e964f7e17a52a58
Duy Phan <duyphan@lecle.co.kr>
2021-08-19 10:45:53 +0700

    chore: add ether add format comment

--- packages/btp-fe/src/connectors/MetaMask/utils.js
+++ packages/btp-fe/src/connectors/MetaMask/utils.js
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers';
 
 export const toChecksumAddress = (address) => {
+  // Ethereum address format
   if (address && address.startsWith('0x') && address.length === 42) {
     return ethers.utils.getAddress(address);
   }
