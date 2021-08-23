9336b7041f4ff06249485ebbdb6b3752c7eb90d9
Tien Do <tiendq@gmail.com>
2021-08-23 11:58:46 +0700

    refactor: update RC reward database, total monthly reward, reward change in last 30 days (#37)

--- packages/dashboard-api/src/modules/relay-candidates/route.js
+++ packages/dashboard-api/src/modules/relay-candidates/route.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const express = require('express');
-const debug = require('debug')('api:relays');
+const debug = require('debug')('api:relay-candidates');
 const { asyncMiddleware, debugLogMiddleware } = require('../../middlewares');
-const { getRelayCandidateList } = require('./controller');
+const { getRelayCandidateList, getMonthlyReward } = require('./controller');
 
 let router = express.Router();
 
 router.use(debugLogMiddleware(debug));
 router.get('/', asyncMiddleware(getRelayCandidateList));
+router.get('/reward', asyncMiddleware(getMonthlyReward));
+
 module.exports = router;
