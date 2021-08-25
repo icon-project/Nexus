57273e9ce61e32afceb5a5e7f5f06093eae8a5ac
Duy Phan <phanduy344@gmail.com>
2021-08-25 12:03:36 +0700

    feat: #262 show the list relay on governance page (#263)
    * feat: update new relay api
    
    * feat: show total registered
    
    * refactor: remove unused methods
    
    * refactor: relays name
    
    * refactor: relays selector
    
    * refactor: remove unused method
    
    * Add defaul value for total registered
    
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>
    Co-authored-by: Luan Vuong <luanvuong@orgos.net>

--- packages/btp-fe/src/store/models/governance.js
+++ packages/btp-fe/src/store/models/governance.js
@@ -1,12 +1,12 @@
-import { getRelayCandidates, getRegisteredRelayLast24h } from 'services/btpServices';
+import { getRelays } from 'services/btpServices';
 
 const governance = {
   state: {
-    relayCandidates: {
+    relays: {
       content: [],
-      rewardChanged30Days: 0,
+      total: 0,
+      registeredLastChange24h: 0,
     },
-    totalRegisteredLast24h: 0,
   },
   reducers: {
     setGovernanceState(state, prop = []) {
@@ -18,49 +18,19 @@ const governance = {
     },
   },
   effects: (dispatch) => ({
-    async getRelayCandidates() {
+    async getRelays({ page, limit }) {
       try {
-        const relayCandidates = await getRelayCandidates();
-        if (!relayCandidates.error) {
-          this.setGovernanceState(['relayCandidates', relayCandidates || {}]);
-        }
-        return relayCandidates;
-      } catch (error) {
-        dispatch.modal.handleError();
-      }
-    },
-    async getRegisteredRelayLast24h() {
-      try {
-        const totalRegisteredLast24h = await getRegisteredRelayLast24h();
-        this.setGovernanceState([
-          'totalRegisteredLast24h',
-          totalRegisteredLast24h?.content.last24hChange || 0,
-        ]);
+        const relays = await getRelays(page, limit);
+        this.setGovernanceState(['relays', relays || {}]);
+        return relays;
       } catch (error) {
         dispatch.modal.handleError();
       }
     },
   }),
   selectors: (slice) => ({
-    selectRelayCandidates() {
-      return slice((state) => state.relayCandidates.content);
-    },
-    selectRewardLast30Days() {
-      return slice((state) => state.relayCandidates.rewardChanged30Days);
-    },
-    selectRegisteredRelayLast24h() {
-      return slice((state) => state.totalRegisteredLast24h);
-    },
-    selectTotalRewardFund() {
-      const getTotalRewardFund = (relays) => {
-        if (!relays || relays.length === 0) return 0;
-        const totalRewardFund = relays.reduce(
-          (total, relay) => total + (relay['monthlyReward'] || 0),
-          0,
-        );
-        return totalRewardFund.toFixed(2);
-      };
-      return slice((state) => getTotalRewardFund(state.relayCandidates.content));
+    selectRelays() {
+      return slice((state) => state.relays);
     },
   }),
 };
