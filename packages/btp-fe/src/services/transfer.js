dd3fbba0cab946dc406cfa712a15e069980f392f
Duy Phan <duyphan@lecle.co.kr>
2021-08-26 10:42:32 +0700

    fix: add error when missing wallet

--- packages/btp-fe/src/services/transfer.js
+++ packages/btp-fe/src/services/transfer.js
@@ -10,6 +10,8 @@ import { wallets } from 'utils/constants';
 
 const getCurrentTransferService = () => (targetWallet) => {
   const { wallet } = store.getState().account;
+  if (!wallet && !targetWallet) throw new Error('Missing wallet');
+
   const iconServices = { transfer, isApprovedForAll, setApprovalForAll, getBalanceOf };
 
   switch (wallet || targetWallet) {
