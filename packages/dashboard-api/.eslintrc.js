1f96a5b642a2e87795bd0cdc52b3cf4b54cb37e1
Lê Duy Minh <duyminh2032000@gmail.com>
2021-09-17 09:19:30 +0700

    test api btp-network

--- packages/dashboard-api/.eslintrc.js
+++ packages/dashboard-api/.eslintrc.js
@@ -1,35 +1,29 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'node': true,
-    'jest': true,
-    'jest/globals': true
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    'jest/globals': true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended', // eslint
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
   ],
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'ecmaVersion': 6
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
   },
-  'plugins': [
-    'jest'
-  ],
-  'rules': {
+  plugins: ['jest'],
+  settings: {
+    jest: {
+      version: 'latest',
+    },
+  },
+  rules: {
     'no-console': 1,
     'no-unused-vars': 1,
-    'indent': [
-      'warn',
-      2
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'warn',
-      'always',
-    ]
-  }
+    indent: ['warn', 2],
+    quotes: ['error', 'single'],
+    semi: ['warn', 'always'],
+  },
 };
--- packages/dashboard-api/tests/integration/api.test.js
+++ packages/dashboard-api/tests/integration/api.test.js
@@ -4,17 +4,69 @@ const request = require('supertest');
 const HttpStatus = require('@tiendq/http-status');
 const app = require('../../src/app/app');
 
+const { helperCompareObject, helperCompareArray } = require('../helpers/helper');
+
+const functions_btpnetwork = require('../defaults/default_btpnetwork');
+
+/***************************************** Test btpnetwork *********************************/
 describe('GET /btpnetwork', () => {
-  test('should return general network information', async () => {
+  test('curl $HOST/v1/btpnetwork | jq', async () => {
     let response = await request(app)
       .get('/v1/btpnetwork')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
       .expect(HttpStatus.OK);
+    // Declare variable is result of function helperCompareObject return boolean value
+    const compareStructure = helperCompareObject.hasEqualStructure(
+      response.body.content,
+      functions_btpnetwork.btpnetwork(),
+    );
+    // check value is true => pass
+    expect(compareStructure).toBeTruthy();
+    //expect(response.body.content).toMatchObject(functions_btpnetwork.btpnetwork());
 
-    expect(response.body.content).toBeDefined();
-  });
-});
+    // set Timeout: 7000 cause bad network
+  }, 7000);
 
-describe('GET /networks', () => {
-  // test('something', async () => {
-  // });
+  test('curl $HOST/v1/btpnetwork?stats=1 | jq', async () => {
+    let response = await request(app)
+      .get('/v1/btpnetwork/?stats=1')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(HttpStatus.OK);
+    const compareStructure = helperCompareObject.hasEqualStructure(
+      response.body.content,
+      functions_btpnetwork.btpnetwork_stats(),
+    );
+    expect(compareStructure).toBeTruthy();
+    //expect(response.body).toMatchObject(functions_btpnetwork.btpnetwork_stats());
+  }, 7000);
+
+  test('curl $HOST/v1/btpnetwork?volumeLast24h=true&mintLast24h=true | jq', async () => {
+    let response = await request(app)
+      .get('/v1/btpnetwork?volumeLast24h=true&mintLast24h=true')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(HttpStatus.OK);
+    const compareStructure = helperCompareObject.hasEqualStructure(
+      response.body.content,
+      functions_btpnetwork.btpnetwork_24h(),
+    );
+    expect(compareStructure).toBeTruthy();
+    // expect(response.body).toMatchObject(functions_btpnetwork.btpnetwork_24h());
+  }, 7000);
+
+  test('curl $HOST/v1/btpnetwork/converter?token=btc&amount=100&convert_to=usd | jq', async () => {
+    let response = await request(app)
+      .get('/v1/btpnetwork/converter?token=btc&amount=100&convert_to=usd')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(HttpStatus.OK);
+    const compareStructure = helperCompareObject.hasEqualStructure(
+      response.body.content,
+      functions_btpnetwork.btpnetwork_converter(),
+    );
+    expect(compareStructure).toBeTruthy();
+    //expect(response.body).toMatchObject(functions_btpnetwork.btpnetwork_converter());
+  }, 7000);
 });
