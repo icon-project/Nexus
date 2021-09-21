41f88e9526750924767cad167b4fc61f09f51901
Lê Duy Minh <duyminh2032000@gmail.com>
2021-09-21 08:46:57 +0700

    test api btpnetwork by using snapshot and expect.any

--- packages/dashboard-api/tests/integration/api.test.js
+++ packages/dashboard-api/tests/integration/api.test.js
@@ -5,6 +5,16 @@ const HttpStatus = require('@tiendq/http-status');
 const app = require('../../src/app/app');
 
 /***************************************** Test btpnetwork *********************************/
+//function add Method expect any for array minted
+function addMethodAnyForMint(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    expect(arr[i]).toMatchSnapshot({
+      mintedVolume: expect.any(Number),
+      networkId: expect.any(String),
+      networkName: expect.any(String),
+    });
+  }
+}
 
 describe('Test btpNetwork', () => {
   test('GET /btpnetwork', async () => {
@@ -23,67 +33,105 @@ describe('Test btpNetwork', () => {
     });
 
     //If snapshots have array, We will use toMatchSnapshot for each element of the array
-    const objectInArray = response.body.content.minted;
-    for (let i = 0; i < objectInArray.length; i++) {
-      expect(objectInArray[i]).toMatchSnapshot({
-        mintedVolume: expect.any(Number),
-        networkId: expect.any(String),
-        networkName: expect.any(String),
+    const objectMintedInArray = response.body.content.minted;
+    addMethodAnyForMint(objectMintedInArray);
+  });
+
+  test('GET /btpnetwork?stats=1', async () => {
+    let response = await request(app)
+      .get('/v1/btpnetwork')
+      .query({
+        stats: 1,
+      })
+      .set('Accept', 'application/json')
+      .expect(HttpStatus.OK);
+
+    expect(response.body.content).toMatchSnapshot({
+      bondedValue: expect.any(Number),
+      fee: {
+        allTimeAmount: expect.any(Array),
+        assets: expect.any(Array),
+        cumulativeAmount: expect.any(Number),
+        currentAmount: expect.any(Number),
+      },
+      minted: expect.any(Array),
+      stats: expect.any(Object),
+      totalNetworks: expect.any(Number),
+      totalTransactions: expect.any(Number),
+      volume: expect.any(Number),
+    });
+
+    const objectMintedInArray = response.body.content.minted;
+
+    addMethodAnyForMint(objectMintedInArray);
+
+    const objectIndexersInArray = response.body.content.stats.indexers;
+    for (let i = 0; i < objectIndexersInArray.length; i++) {
+      expect(objectIndexersInArray[i]).toMatchSnapshot({
+        block_height: expect.any(Number),
+        name: expect.any(String),
+        network_id: expect.any(String),
+        updated_time: expect.any(String),
       });
     }
   });
 
-  // test('GET /btpnetwork?stats=1', async () => {
-  //   let response = await request(app)
-  //     .get('/v1/btpnetwork')
-  //     .query({
-  //       stats: 1
-  //     })
-  //     .set('Accept', 'application/json')
-  //     .expect(HttpStatus.OK);
-  //     const keys = Object.keys(response.body.content);
-  //     let newObject;
+  test('GET /btpnetwork?volumeLast24h=true&mintLast24h=true', async () => {
+    let response = await request(app)
+      .get('/v1/btpnetwork')
+      .query({
+        volumeLast24h: true,
+        mintLast24h: true,
+      })
+      .set('Accept', 'application/json')
+      .expect(HttpStatus.OK);
 
-  // expect(response.body.content).toMatchSnapshot({
-  //   "bondedValue": expect.any(Number),
-  //   "fee": {
-  //     "allTimeAmount": expect.any(Array),
-  //     "assets": expect.any(Array),
-  //     "cumulativeAmount": expect.any(Number),
-  //     "currentAmount": expect.any(Number),
-  //   },
-  //   "minted": expect.any(Array),
-  //   "stats": expect.any(Object),
-  //   "totalNetworks": expect.any(Number),
-  //   "totalTransactions":  expect.any(Number),
-  //   "volume":  expect.any(Number),
-  // })
-  // });
+    expect(response.body.content).toMatchSnapshot({
+      bondedValue: expect.any(Number),
+      fee: {
+        allTimeAmount: expect.any(Array),
+        assets: expect.any(Array),
+        cumulativeAmount: expect.any(Number),
+        currentAmount: expect.any(Number),
+      },
+      mintVolumeLast24hChange: expect.any(Number),
+      minted: expect.any(Array),
+      totalNetworks: expect.any(Number),
+      totalTransactions: expect.any(Number),
+      volume: expect.any(Number),
+      volumeLast24hChange: expect.any(Number),
+    });
 
-  // test('curl $HOST/v1/btpnetwork?volumeLast24h=true&mintLast24h=true | jq', async () => {
-  //   let response = await request(app)
-  //     .get('/v1/btpnetwork?volumeLast24h=true&mintLast24h=true')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(HttpStatus.OK);
-  //   const compareStructure = helperCompareObject.hasEqualStructure(
-  //     response.body,
-  //     functions_btpnetwork.btpnetwork_24h(),
-  //   );
-  //   expect(compareStructure).toBeTruthy();
-  // });
+    const objectMintedInArray = response.body.content.minted;
 
-  // test('curl $HOST/v1/btpnetwork/converter?token=btc&amount=100&convert_to=usd | jq', async () => {
-  //   let response = await request(app)
-  //     .get('/v1/btpnetwork/converter?token=btc&amount=100&convert_to=usd')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(HttpStatus.OK);
-  //   const compareStructure = helperCompareObject.hasEqualStructure(
-  //     response.body,
-  //     functions_btpnetwork.btpnetwork_converter(),
-  //   );
-  //   expect(compareStructure).toBeTruthy();
-  //   //expect(response.body).toMatchObject(functions_btpnetwork.btpnetwork_converter());
-  // });
+    addMethodAnyForMint(objectMintedInArray);
+  });
+  test('GET /btpnetwork/converter?token=btc&amount=100&convert_to=usd', async () => {
+    let response = await request(app)
+      .get('/v1/btpnetwork')
+      .query({
+        token: 'btc',
+        amount: 100,
+        convert_to: 'usd',
+      })
+      .set('Accept', 'application/json')
+      .expect(HttpStatus.OK);
+
+    expect(response.body.content).toMatchSnapshot({
+      bondedValue: expect.any(Number),
+      fee: {
+        allTimeAmount: expect.any(Array),
+        assets: expect.any(Array),
+        cumulativeAmount: expect.any(Number),
+        currentAmount: expect.any(Number),
+      },
+      minted: expect.any(Array),
+      totalNetworks: expect.any(Number),
+      totalTransactions: expect.any(Number),
+      volume: expect.any(Number),
+    });
+
+    const objectMintedInArray = response.body.content.minted;
+    addMethodAnyForMint(objectMintedInArray);
+  });
 });
