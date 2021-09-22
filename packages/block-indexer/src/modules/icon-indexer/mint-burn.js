27fb80c38e06785535547fb6eed93f3fee501767
Sang Lai <Corgi@Sangs-MacBook-Pro.local>
2021-09-22 17:58:56 +0700

    refactor(mint-burn): refactor duplicated code

--- packages/block-indexer/src/modules/icon-indexer/mint-burn.js
+++ packages/block-indexer/src/modules/icon-indexer/mint-burn.js
@@ -7,7 +7,9 @@ const { IconConverter } = require('icon-sdk-js');
 const { decode } = require('rlp');
 
 const ZERO_ADDRESS = 'hx0000000000000000000000000000000000000000';
-const TRANSFER_BATCH_PROTOTYPE  = 'TransferBatch(Address,Address,Address,bytes,bytes)';
+const TRANSFER_BATCH_PROTOTYPE = 'TransferBatch(Address,Address,Address,bytes,bytes)';
+const MINT = 'mint';
+const BURN = 'burn';
 
 async function getTokensInfo(idEncode, valueEncode) {
   const valueDecoded = decode(valueEncode);
@@ -17,34 +19,39 @@ async function getTokensInfo(idEncode, valueEncode) {
   const name = await getTokenNameById(id);
   const value = IconConverter.toNumber('0x' + valueDecoded[0].toString('hex'));
 
-  console.log(id);
   return {
     tokenValue: value,
     tokenName: name,
-    tokenId: id
-  }
+    tokenId: id,
+  };
 }
 
 async function handleMintBurnEvents(txResult, transaction) {
-  if (0 === txResult.eventLogs.length || 1 !== txResult.status || TRANSFER_BATCH_PROTOTYPE !== txResult.eventLogs[0].indexed[0])
+  if (
+    0 === txResult.eventLogs.length ||
+    1 !== txResult.status ||
+    TRANSFER_BATCH_PROTOTYPE !== txResult.eventLogs[0].indexed[0]
+  )
     return false;
 
   try {
     const eventObj = await getMintBurnEvent(txResult, transaction);
 
-    if(!eventObj.tokenName) {
+    if (!eventObj.tokenName) {
       logger.debug('handleMintBurnEvents Token not registered with Icon blockchain');
       return false;
     }
 
-    if(ZERO_ADDRESS === txResult.eventLogs[0].indexed[2]) { // mint when _from value is ZERO
-      const totalMintToken = await getTotalTokenMintAmount(eventObj.tokenName);
+    if (ZERO_ADDRESS === txResult.eventLogs[0].indexed[2]) {
+      // mint when _from value is ZERO
+      const totalMintToken = await getTotalTokenAmount(eventObj.tokenName, MINT);
 
-      await saveMintToken(eventObj, totalMintToken);
-    } else if (ZERO_ADDRESS === txResult.eventLogs[0].indexed[3]) { // burn when _to value is ZERO
-      const totalBurnToken = await getTotalTokenBurnAmount(eventObj.tokenName);
+      await saveToken(eventObj, totalMintToken, MINT);
+    } else if (ZERO_ADDRESS === txResult.eventLogs[0].indexed[3]) {
+      // burn when _to value is ZERO
+      const totalBurnToken = await getTotalTokenAmount(eventObj.tokenName, BURN);
 
-      await saveBurnToken(eventObj, totalBurnToken);
+      await saveToken(eventObj, totalBurnToken, BURN);
     } else {
       return false;
     }
@@ -79,46 +86,62 @@ async function getMintBurnEvent(txResult, transaction) {
   }
 }
 
-async function getTotalTokenMintAmount(name) {
-  const { rows } = await pgPool.query('SELECT total_token_amount FROM minted_tokens WHERE token_name = $1 ORDER BY create_at DESC LIMIT 1', [name]);
-  return rows[0] ? Number(rows[0].total_token_amount) : 0;
-}
-
-async function getTotalTokenBurnAmount(name) {
-  const { rows } = await pgPool.query('SELECT total_token_amount FROM burned_tokens WHERE token_name = $1 ORDER BY create_at DESC LIMIT 1', [name]);
-  return rows[0] ? Number(rows[0].total_token_amount) : 0;
-}
-
-async function getTokenNameById(id) {
-  const { rows } = await pgPool.query('SELECT token_name FROM token_info WHERE token_id = $1', [id]);
-  return rows[0] ? rows[0].token_name : false;
-}
-
-async function saveMintToken(mintObj, totalToken) {
+async function getTotalTokenAmount(tokenName, tokenType) {
   try {
-    preSave(mintObj);
-
-    const totalTokenAmount = totalToken + mintObj.tokenValue;
-    const query = 'INSERT INTO minted_tokens (id, network_id, token_name, token_value, total_token_amount, block_time, tx_hash, token_id, mint_to, create_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW())';
-    const values = [mintObj.id, mintObj.networkId, mintObj.tokenName, mintObj.tokenValue, totalTokenAmount, mintObj.blockTime, mintObj.txHash, mintObj.tokenId, mintObj.to];
-
-    await pgPool.query(query, values);
+    let tableName = 'minted_tokens';
+    if (BURN == tokenType) {
+      tableName = 'burned_tokens';
+    }
+    const {
+      rows,
+    } = await pgPool.query(
+      `SELECT total_token_amount FROM ${tableName} WHERE token_name = $1 ORDER BY create_at DESC LIMIT 1`,
+      [tokenName],
+    );
+    return rows[0] ? Number(rows[0].total_token_amount) : 0;
   } catch (error) {
-    logger.error('saveMintToken failed save mint value', { error });
+    logger.error(`getTotalTokenAmount failed get ${tokenType} value`, { error });
   }
 }
 
-async function saveBurnToken(burnObj, totalToken) {
-  try {
-    preSave(burnObj);
+async function getTokenNameById(id) {
+  const { rows } = await pgPool.query('SELECT token_name FROM token_info WHERE token_id = $1', [
+    id,
+  ]);
+  return rows[0] ? rows[0].token_name : false;
+}
 
-    const totalTokenAmount = totalToken + burnObj.tokenValue;
-    const query = 'INSERT INTO burned_tokens (id, network_id, token_name, token_value, total_token_amount, block_time, tx_hash, token_id, burn_from, create_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW())';
-    const values = [burnObj.id, burnObj.networkId, burnObj.tokenName, burnObj.tokenValue, totalTokenAmount, burnObj.blockTime, burnObj.txHash, burnObj.tokenId, burnObj.from];
+async function saveToken(object, totalToken, tokenType) {
+  try {
+    preSave(object);
+
+    let tableName = 'minted_tokens';
+    let dymamicColumn = 'mint_to';
+
+    if (BURN == tokenType) {
+      tableName = 'burned_tokens';
+      dymamicColumn = 'burn_from';
+    }
+
+    const totalTokenAmount = totalToken + object.tokenValue;
+    const query = `
+    INSERT INTO ${tableName} (id, network_id, token_name, token_value, total_token_amount, block_time, tx_hash, token_id, ${dymamicColumn}, create_at)
+           VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW())`;
+    const values = [
+      object.id,
+      object.networkId,
+      object.tokenName,
+      object.tokenValue,
+      totalTokenAmount,
+      object.blockTime,
+      object.txHash,
+      object.tokenId,
+      object.to,
+    ];
 
     await pgPool.query(query, values);
   } catch (error) {
-    logger.error('saveBurnToken failed save mint value', { error });
+    logger.error(`saveToken failed save ${tokenType} value`, { error });
   }
 }
 
@@ -129,11 +152,10 @@ async function saveBurnToken(burnObj, totalToken) {
 function preSave(data) {
   if (!data.id) {
     data.id = uuidv4();
-    data.createAt = Math.floor(new Date().getTime());
   }
 }
 
 module.exports = {
   handleMintBurnEvents,
-  getMintBurnEvent
+  getMintBurnEvent,
 };
