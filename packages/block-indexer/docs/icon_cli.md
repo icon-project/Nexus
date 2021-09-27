ab4b195a098d00f34a90e6984b4b5d0151f9e5b4
Tien Do <tiendq@gmail.com>
2021-09-27 13:05:54 +0700

    fix: check to only register native coin (#379)

--- packages/block-indexer/docs/icon_cli.md
+++ packages/block-indexer/docs/icon_cli.md
@@ -1,5 +1,7 @@
-./goloop rpc txbyhash 0xa8c98c4b4d1460ef783c3f47a2e5dba5caae57dea398d90de36e65d4dbc78ad8 --uri http://localhost:9082/api/v3
+goloop rpc txbyhash 0x3f09bf2d587fb0b38ad7ce1d6613b01ba77f14e28bbbdecbf3612f5fe5d27acf --uri http://localhost:9080/api/v3/icon
 
-./goloop rpc txresult 0xa8c98c4b4d1460ef783c3f47a2e5dba5caae57dea398d90de36e65d4dbc78ad8 --uri http://localhost:9082/api/v3
+goloop rpc txresult 0x8fabd9ad200497d59f0e2528772d2df8e98093ca3645b821d78286056e9a7c9e --uri http://localhost:9080/api/v3/icon
 
 ./goloop rpc blockbyhash 0x7b8275119cb9735ac04f6f1da991bb04b8a4605f5ae2bdb604ef70bb09deaba0 --uri http://localhost:9082/api/v3
+
+goloop rpc scoreapi --uri http://localhost:9080/api/v3/icon cx77622c6d0bcd80048eb159aa99fd30df2f38c97f
--- packages/block-indexer/src/modules/icon-indexer/token-register.js
+++ packages/block-indexer/src/modules/icon-indexer/token-register.js
@@ -10,30 +10,31 @@ const httpProvider = new HttpProvider(process.env.ICON_API_URL);
 const iconService = new IconService(httpProvider);
 
 async function handleTokenRegister(txResult, transaction) {
-  if (transaction.data.method) {
-    if('register' === transaction.data.method && 1 === txResult.status) {
+  // Handle native coin.
+  if (1 === txResult.status && process.env.ICON_NATIVE_COIN_BSH_ADDRESS === transaction.to) {
+    if ('register' === transaction.data.method) {
       const id = await getTokenId(transaction.data.params._name);
 
       const tokenObj = {
         tokenId: id,
-        tokenName: transaction.data.params._name
+        tokenName: transaction.data.params._name,
+        txHash: transaction.txHash,
+        contractAddress: transaction.to
       };
 
-      await saveTokenInfo(tokenObj, txResult);
+      await saveTokenInfo(tokenObj);
     }
   }
 }
 
-async function saveTokenInfo(tokenObj, txResult) {
+async function saveTokenInfo(tokenObj) {
   try {
-    preSave(tokenObj);
-
-    const query = 'INSERT INTO token_info (id, network_id, token_id, token_name, tx_hash, create_at) VALUES ($1, $2, $3, $4, $5, NOW())';
-    const values = [tokenObj.id, process.env.ICON_NETWORK_ID, tokenObj.tokenId, tokenObj.tokenName, txResult.txHash];
+    const query = 'INSERT INTO token_info (id, network_id, token_id, token_name, tx_hash, create_at, contract_address, token_address) VALUES ($1, $2, $3, $4, $5, NOW(), $7, $8)';
+    const values = [uuidv4(), process.env.ICON_NETWORK_ID, tokenObj.tokenId, tokenObj.tokenName, tokenObj.txHash, tokenObj.contractAddress, tokenObj.contractAddress];
 
     await pgPool.query(query, values);
   } catch (error) {
-    logger.error('saveTokenInfo failed ', { error });
+    logger.error('saveTokenInfo failed %O', error);
   }
 }
 
@@ -47,16 +48,7 @@ async function getTokenId(name) {
     .build();
 
   const tokenId = await iconService.call(txObject).execute();
-  debug('Token Name %O Token ID: %O', name, tokenId);
-
   return tokenId;
-}
-
-function preSave(data) {
-  if (!data.id) {
-    data.id = uuidv4();
-    data.createAt = Math.floor(new Date().getTime());
-  }
 }
 
 module.exports = {
