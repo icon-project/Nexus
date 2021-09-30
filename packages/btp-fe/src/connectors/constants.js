5ec3ac564d43805ee1934defe596af3289763960
Duy Phan <phanduy344@gmail.com>
2021-09-30 08:14:45 +0700

    feat: #399 allow to update network config via local storage (#400)
    Co-authored-by: Duy Phan <duyphan@lecle.co.kr>

--- packages/btp-fe/src/connectors/constants.js
+++ packages/btp-fe/src/connectors/constants.js
@@ -25,22 +25,26 @@ export const allowedNetworkIDs = {
   metamask: { '0x501': connectedNetWorks.moonbeam, '0x4': 'Rinkeby' },
 };
 
+const customNetworks = localStorage.getItem('NETWORK_CONFIG');
+const { icon, moonbeam } = customNetworks ? JSON.parse(customNetworks) : { icon: {}, moonbeam: {} };
+
 export const MOON_BEAM_NODE = {
-  RPCUrl: 'http://54.251.114.18:9933',
-  BSHCore: '0x7d4567B7257cf869B01a47E8cf0EDB3814bDb963',
-  networkAddress: '0x501.pra',
-  gasLimit: '6691B7',
+  RPCUrl: moonbeam.endpoint || 'http://54.251.114.18:9933',
+  BSHCore: moonbeam.BSHCore || '0x7d4567B7257cf869B01a47E8cf0EDB3814bDb963',
+  networkAddress: moonbeam.networkAddress || '0x501.pra',
+  gasLimit: moonbeam.gasLimit || '6691B7',
 };
+console.log('MOON_BEAM_NODE', MOON_BEAM_NODE);
 
 // https://www.icondev.io/docs/testnet
 export const NETWORKS = {
   cloud: {
     name: connectedNetWorks.icon,
-    endpoint: 'http://54.251.114.18:9080/api/v3',
-    nid: '0x58eb1c',
-    networkAddress: '0x58eb1c.icon',
-    irc31token: 'cxbac7add3adffb4ca43426ede92662da7dcc0453f',
-    BSHAddress: 'cx489ed02580ce5cab57925317373310205417c2b7', // used to get the BTP fee from getBTPfee()
+    endpoint: icon.endpoint || 'http://54.251.114.18:9080/api/v3',
+    nid: icon.nid || '0x58eb1c',
+    networkAddress: icon.networkAddress || '0x58eb1c.icon',
+    irc31token: icon.irc31token || 'cxbac7add3adffb4ca43426ede92662da7dcc0453f',
+    BSHAddress: icon.BSHAddress || 'cx489ed02580ce5cab57925317373310205417c2b7', // used to get the BTP fee from getBTPfee()
   },
   sejong: {
     name: 'Sejong Testnet',
@@ -53,6 +57,7 @@ export const NETWORKS = {
     nid: '1',
   },
 };
+console.log('ICON_NODE', NETWORKS.cloud);
 
 const testnet = localStorage.getItem('dev');
 export const currentICONexNetwork =
