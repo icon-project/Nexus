e202c342d81bcabf647d89ca66be4011051654d2
Duy Phan <duyphan@orgos.net>
2021-11-09 14:44:34 +0700

    refactor: add Approval proptype

--- packages/btp-fe/src/components/TransferBox/Approval.jsx
+++ packages/btp-fe/src/components/TransferBox/Approval.jsx
@@ -1,6 +1,7 @@
 import { memo, useEffect, useState } from 'react';
 import styled from 'styled-components/macro';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import PropTypes from 'prop-types';
 
 import { useDispatch } from 'hooks/useRematch';
 import { useListenForSuccessTransaction } from 'hooks/useListenForSuccessTransaction';
@@ -210,3 +211,24 @@ export const Approval = memo(
 );
 
 Approval.displayName = 'Approval';
+
+Approval.propTypes = {
+  /** Set step for transfer box */
+  setStep: PropTypes.func,
+  /** Transfer box values */
+  values: PropTypes.shape({ recipient: PropTypes.string, tokenAmount: PropTypes.number }),
+  sendingInfo: PropTypes.shape({
+    token: PropTypes.string,
+    network: PropTypes.string,
+  }),
+  account: PropTypes.shape({
+    currentNetwork: PropTypes.string,
+    unit: PropTypes.string,
+  }),
+  /** react-final-form object */
+  form: PropTypes.object,
+  /** Is the current display screen */
+  isCurrent: PropTypes.bool,
+  /** Token in USD rate */
+  usdRate: PropTypes.number,
+};
