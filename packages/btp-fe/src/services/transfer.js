97930cd28b97e1e0a870e9f9378ea9e44ec14b77
Duy Phan <duyphan@orgos.net>
2021-12-15 09:50:40 +0700

    refactor: transfer handler

--- packages/btp-fe/src/services/transfer.js
+++ packages/btp-fe/src/services/transfer.js
@@ -1,48 +1,40 @@
 import store from 'store';
-import { wallets, getPairedNetwork, pairedNetworks } from 'utils/constants';
+import { wallets, connectedNetWorks } from 'utils/constants';
 
 import * as ICONServices from 'connectors/ICONex/ICONService';
 import * as BSCServices from 'connectors/MetaMask/services/BSCServices';
 import * as MoonbeamServices from 'connectors/MetaMask/services/MoonbeamServices';
 import * as NEARServices from 'connectors/NEARWallet';
 
-export const getCurrentTransferService = () => (targetWallet) => {
-  const { wallet } = store.getState().account;
-  if (!wallet && !targetWallet) throw new Error('Missing wallet');
+export const getCurrentTransferService = () => (curentWallet, currentNetwork) => {
+  const { wallet, currentNetwork: network } = store.getState().account;
+  if (!wallet && !curentWallet) throw new Error('Missing wallet');
+  if (!network && !currentNetwork) throw new Error('Missing network');
 
-  const currentPairedNetworks = getPairedNetwork();
-  if (!currentPairedNetworks) throw new Error('Missing chains');
-
-  switch (wallet || targetWallet) {
+  switch (wallet || curentWallet) {
     case wallets.metamask:
-      switch (currentPairedNetworks) {
-        case pairedNetworks['ICON-Moonbeam']:
+      // There are 2 network using Metamask now: Moonbeam & BSC
+      switch (network || currentNetwork) {
+        case connectedNetWorks.moonbeam:
           return MoonbeamServices;
 
-        case pairedNetworks['ICON-BSC']:
+        case connectedNetWorks.bsc:
           return BSCServices;
+        default:
+          console.log('No matching network');
+          break;
       }
       break;
+
     case wallets.iconex:
       return ICONServices;
+
     case wallets.near:
       return NEARServices;
 
     default:
       console.log('No matching wallet service');
       break;
-  }
-
-  if ((wallet || targetWallet) === wallets.metamask) {
-    switch (currentPairedNetworks) {
-      case pairedNetworks['ICON-Moonbeam']:
-        return MoonbeamServices;
-
-      case pairedNetworks['ICON-BSC']:
-        return BSCServices;
-    }
-  } else {
-    return ICONServices;
   }
 };
 
