646f340b6fa4d67cd1cbf40350a398b668282065
Duy Phan <duyphan@orgos.net>
2021-12-16 14:10:10 +0700

    refactor: checking approval

--- packages/btp-fe/src/components/TransferCard/handleCheckingApproval.js
+++ packages/btp-fe/src/components/TransferCard/handleCheckingApproval.js
@@ -1,9 +1,14 @@
 import { TransferApproval } from 'components/NotificationModal/TransferApproval';
-import { isICONAndBSHPaired } from 'utils/constants';
+import { getPairedNetwork, pairedNetworks } from 'utils/constants';
 import { getService } from 'services/transfer';
 
 export default async (setStep, isSendingNativeCoin, currentNetwork, openModal, setDisplay) => {
-  if (isSendingNativeCoin || isICONAndBSHPaired()) {
+  if (
+    isSendingNativeCoin ||
+    // Not implemented checking approval for BSC and NEAR
+    // So, just bypass for now.
+    [pairedNetworks['ICON-NEAR'], pairedNetworks['ICON-BSC']].includes(getPairedNetwork())
+  ) {
     setStep(1);
   } else {
     const result = await getService(currentNetwork).isApprovedForAll();
