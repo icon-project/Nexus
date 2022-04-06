309974a4f71a92522ab7c97a3d04ecbd8563b4b1
Duy Phan <duyphan@orgos.net>
2022-04-06 09:57:53 +0700

    feat: getting chain configs from env

---
+++ packages/btp-fe/src/connectors/chainConfigs.js
@@ -0,0 +1,10 @@
+const chainConfigs = {};
+Object.keys(process.env).forEach((e) => {
+  if (e.startsWith('REACT_APP_CHAIN')) {
+    const chainName = e.split('_')[3];
+    if (!chainConfigs[chainName]) chainConfigs[chainName] = {};
+    chainConfigs[chainName][e.split(chainName + '_')[1]] = process.env[e];
+  }
+});
+
+console.log('🚀 ~ file: chainConfigs.js ~ line 4 ~ chainConfigs', chainConfigs);
