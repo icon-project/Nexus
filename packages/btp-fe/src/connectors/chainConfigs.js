94fed5ac40463224d4a7e8b0a872564ca3d4b4dd
Duy Phan <duyphan@orgos.net>
2022-04-07 09:30:58 +0700

    fix: set methods default value

--- packages/btp-fe/src/connectors/chainConfigs.js
+++ packages/btp-fe/src/connectors/chainConfigs.js
@@ -6,7 +6,7 @@ Object.keys(process.env).forEach((e) => {
   if (e.startsWith('REACT_APP_CHAIN')) {
     const chainName = e.split('_')[3];
     if (!chainConfigs[chainName]) {
-      chainConfigs[chainName] = { id: chainName };
+      chainConfigs[chainName] = { id: chainName, methods: {} };
     }
     chainConfigs[chainName][e.split(chainName + '_')[1]] = process.env[e];
   }
