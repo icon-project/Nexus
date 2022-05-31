7386b2ff617997af9360fb36d20099f3655c0ef7
Dan <dantn93@gmail.com>
2022-05-31 09:06:45 +0700

    refactor: change indexer status message

--- packages/dashboard-api/src/modules/health-check/model.js
+++ packages/dashboard-api/src/modules/health-check/model.js
@@ -7,20 +7,18 @@ const moment = require('moment');
 const indexerCommandHealthCheck = async () => {
   try {
     const healthCheckParams = parseIndexerHealthCheckPeriod();
-    let message = '';
-    await Promise.all(healthCheckParams.map(async e => {
+    const messages = await Promise.all(healthCheckParams.map(async e => {
       const indexerStat = await getIndexerStatByNetworkId(e.networkId);
       const updateAt = moment(indexerStat.updateAt);
       const checkTime = moment().subtract(e.period, 'minutes');
       // If indexerStat.updatAt < currentTime - period => Push error to slack
       if (!updateAt.isValid() || updateAt.isBefore(checkTime)) {
-        message += BLOCK_INDEXER_STOPPED(indexerStat.blockHeight, indexerStat.name) + '\n';
+        return BLOCK_INDEXER_STOPPED(indexerStat.blockHeight, indexerStat.name);
       } else {
-        message += BLOCK_INDEXER_HEALTHY(indexerStat.blockHeight, indexerStat.name) + '\n';
+        return BLOCK_INDEXER_HEALTHY(indexerStat.blockHeight, indexerStat.name);
       }
-      return null;
     }));
-    return message;
+    return messages.join('\n');
   } catch (error) {
     logger.error(error);
     return null;
