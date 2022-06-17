d6b57fd6a1c3ed2fe2096d674f99ed59b21694bc
Dan <dantn93@gmail.com>
2022-06-17 16:20:04 +0700

    test: update block-indexer's test case

--- packages/block-indexer/tests/transaction.test.js
+++ packages/block-indexer/tests/transaction.test.js
@@ -22,55 +22,61 @@ jest.mock('pg', () => {
 });
 
 const transferNativeCoinTx = {
-  timestamp: 1625217594463911,
-  value: '1e+21',
-  nid: { c: ['3'] },
-  stepLimit: '10000000000',
-  from: 'hxb6b5791be0b5ef67063b3c10b840fb81514db2fd',
-  to: process.env.ICON_BMC_ADDRESS,
-  signature:
-    'X01Gnp/G/Bp3UnHw9oqk1lvWuBcsL9dWNQH/4BE65NY2V259BWIvSrIZGFX2p+ITFmjMNi5ymR2KPAhdpkfhRgA=',
+  timestamp: 1654697443093000,
+  value: { s: 1, e: 18, c: [10000] },
+  nid: { s: 1, e: 0, c: [7] },
+  stepLimit: { s: 1, e: 6, c: [2000000] },
+  nonce: { s: 1, e: 0, c: [1] },
+  from: 'hxe52720d9125586e64c745bf3c2c1917dbb46f9ba',
+  to: 'cxe3fbc6d12fc842935ad68991b5247ea821584002',
+  signature: 'xp/8Cgbvq52sDe+gu8536Vy1E3HytUym85GE4ZF8MI8KRnvE452aiddZzmuHvmqeuoJUXhIievGiUA14qj1smwE=',
   dataType: 'call',
   data: {
     method: 'transferNativeCoin',
     params: {
-      _to: 'btp://0x03.icon/hxdcdbf343de48a378ce68ccef3b380ad45b5f21e9'
+      _to: 'btp://0x61.bsc/0x4bb718Cb404787BF97bB012Bb08096602fb9544B'
     }
   },
-  version: '3',
-  txHash: '0xa856a2afef583be6fa60e00e0e7b3b24713d56ee791867066785080bd84a1754'
+  version: { s: 1, e: 0, c: [3] },
+  txHash: '0xcafb7f8a63c307e440db3fc5ee2d8214f5e9eb1097c17c888e081122eaa66b48'
 };
 
 const txResult = {
   status: 1,
-  to: process.env.ICON_BMC_ADDRESS,
-  txHash: '0xa856a2afef583be6fa60e00e0e7b3b24713d56ee791867066785080bd84a1754',
-  txIndex: 0,
-  blockHeight: 129858,
-  blockHash: '0xa5b0c06ffef783d7ec0c4e49966dede727f1a365c7701683388d610f49b94d96',
-  cumulativeStepUsed: '744603',
-  stepUsed: { c: [744603] },
-  stepPrice: { c: [12500000000] },
+  to: 'cxe3fbc6d12fc842935ad68991b5247ea821584002',
+  txHash: '0xcafb7f8a63c307e440db3fc5ee2d8214f5e9eb1097c17c888e081122eaa66b48',
+  txIndex: 1,
+  blockHeight: 7780042,
+  blockHash: '0x3150f71e5b448626e105a72c53f73c0d518beb4f8da6c49714e12c54efafc092',
+  cumulativeStepUsed: { s: 1, e: 6, c: [1291748] },
+  stepUsed: { s: 1, e: 6, c: [1291748] },
+  stepPrice: { s: 1, e: 10, c: [12500000000] },
   eventLogs: [
     {
-      scoreAddress: 'cx0c9f31cd4436d29680b6551a76449020186eeec1',
+      scoreAddress: 'cxf3f7533eead8941a5655a90379caf9d121fb2725',
       indexed: [
-        'TransferStart(Address,str,int,bytes)',
-        'hxb6b5791be0b5ef67063b3c10b840fb81514db2fd'
+        'Message(str,int,bytes)',
+        'btp://0x61.bsc/0x121A1AAd623AF68162B1bD84c44234Bc3a3562a9',
+        '0x77'
       ],
       data: [
-        'hxdcdbf343de48a378ce68ccef3b380ad45b5f21e9',
-        '0x1',
-        '0xd0cf83494358893635c9adc5dea0000000'
+        '0xf8f0b8396274703a2f2f3078372e69636f6e2f637866336637353333656561643839343161353635356139303337396361663964313231666232373235b8396274703a2f2f307836312e6273632f3078313231413141416436323341463638313632423162443834633434323334426333613335363261398a6e6174697665636f696e57b86cf86a00b867f865aa687865353237323064393132353538366536346337343562663363326331393137646262343666396261aa307834626237313843623430343738374246393762423031324262303830393636303266623935343442cecd83494358880dbd2fc137a30000'
       ]
     },
     {
-      scoreAddress: 'cx0c9f31cd4436d29680b6551a76449020186eeec1',
-      indexed: ['TransferEnd(Address,int,int,str)', 'hxb6b5791be0b5ef67063b3c10b840fb81514db2fd'],
-      data: ['0x1', '0x0', 'Transfer Success']
+      scoreAddress: 'cxe3fbc6d12fc842935ad68991b5247ea821584002',
+      indexed: [
+        'TransferStart(Address,str,int,bytes)',
+        'hxe52720d9125586e64c745bf3c2c1917dbb46f9ba'
+      ],
+      data: [
+        'btp://0x61.bsc/0x4bb718Cb404787BF97bB012Bb08096602fb9544B',
+        '0x57',
+        '0xd6d583494358880dbd2fc137a30000872386f26fc10000'
+      ]
     }
   ],
-  logsBloom: '0x000000000000000000000000000000000000'
+  logsBloom: '0x40000000000000000000000000000000000100200000000000000000000000000000000000020000000000000000000000010000000000000000000000000000000000000008040000000000000000200000200000000000000000000000000000000000000000000000000000040000000000000000000000000000080000002000000000000000000000000000020000000000000000000000000000002000000000000000000000000000000000000800000000000000000000004000010000000000000000000001000000000000000000000000000000000400000000000000000000000000000000000000000000000000001000800000000000000000'
 };
 
 let pool = null;
@@ -92,64 +98,40 @@ test('should create new transaction with TransferStart event', async () => {
 
   await iconTxHandler.handleTransactionEvents(txResult, transferNativeCoinTx);
 
-  expect(pool.query).toBeCalledTimes(3);
+  expect(pool.query).toBeCalledTimes(4);
+  expect(pool.query).nthCalledWith(2,
+    `SELECT * FROM  transactions WHERE token_name = $1 ORDER BY update_at DESC LIMIT 1`, ["ICX"]
+  );
   expect(pool.query).nthCalledWith(3,
     `INSERT INTO transactions (
       from_address, token_name, serial_number,
       value, to_address,
       tx_hash, block_time, network_id, btp_fee,
-      network_fee, status, total_volume, create_at,
-      update_at, contract_address)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, NOW(), NOW(), $13)`,
+      network_fee, status, total_volume, wps_data,
+      contract_address, log_id1)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`,
     [
-      txResult.eventLogs[0].indexed[1],
-      'ICX',
-      1,
-      1000,
-      txResult.eventLogs[0].data[0],
-      txResult.txHash,
-      Math.floor(transferNativeCoinTx.timestamp / 1000),
-      '0x58eb1c',
+      "hxe52720d9125586e64c745bf3c2c1917dbb46f9ba",
+      "ICX",
+      87,
+      0.99,
+      "btp://0x61.bsc/0x4bb718Cb404787BF97bB012Bb08096602fb9544B",
+      "0xcafb7f8a63c307e440db3fc5ee2d8214f5e9eb1097c17c888e081122eaa66b48",
+      1654697443093,
+      "0x7",
+      0.01,
+      0.01614685,
       0,
-      0.0093075375,
-      0,
-      1000,
-      txResult.eventLogs[0].scoreAddress
+      0.99,
+      undefined,
+      "cxe3fbc6d12fc842935ad68991b5247ea821584002",
+      ""
     ]
+
+  );
+  expect(pool.query).nthCalledWith(4,
+    `SELECT name FROM networks WHERE id = $1`,
+    ["0x7"]
   );
 });
 
-test('should update total volume when creating new transaction', async () => {
-  pool.query.mockResolvedValueOnce({
-    rows: [{ total_volume: 500 }],
-    rowCount: 1
-  });
-
-  await iconTxHandler.handleTransactionEvents(txResult, transferNativeCoinTx);
-
-  expect(pool.query).toBeCalledTimes(2);
-  expect(pool.query).nthCalledWith(2,
-    `INSERT INTO transactions (
-      from_address, token_name, serial_number,
-      value, to_address,
-      tx_hash, block_time, network_id, btp_fee,
-      network_fee, status, total_volume, create_at,
-      update_at, contract_address)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, NOW(), NOW(), $13)`,
-    [
-      txResult.eventLogs[0].indexed[1],
-      'ICX',
-      1,
-      1000,
-      txResult.eventLogs[0].data[0],
-      txResult.txHash,
-      Math.floor(transferNativeCoinTx.timestamp / 1000),
-      '0x58eb1c',
-      0,
-      0.0093075375,
-      0,
-      1500,
-      txResult.eventLogs[0].scoreAddress
-    ]
-  );
-});
