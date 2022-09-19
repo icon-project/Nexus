8868c6d2db10f23148e44334547b07238a05a7dc
Sang Lai <sanglt.cdn@gmail.com>
2022-09-19 14:38:06 +0700

    feat: integrate with near protocal

--- packages/block-indexer/src/modules/common/addresses.js
+++ packages/block-indexer/src/modules/common/addresses.js
@@ -7,10 +7,12 @@ function setBMCAddressMap() {
   BMC_ADDRESS_MAP.set(process.env.ICON_BMC_ADDRESS, 'BSC');
   BMC_ADDRESS_MAP.set(process.env.ICON_WPS_BMC, 'WPS');
   BMC_ADDRESS_MAP.set(process.env.ICON_HMNY_BMC_ADDRESS, 'HARMONY');
+  BMC_ADDRESS_MAP.set(process.env.ICON_NEAR_BMC_ADDRESS, 'NEAR');
 }
 function setBSHAddressMap() {
   BSH_ADDRESS_MAP.set(process.env.ICON_NATIVE_COIN_BSH_ADDRESS, 'BSC');
   BSH_ADDRESS_MAP.set(process.env.ICON_NATIVE_COIN_HMNY_BSH_ADDRESS, 'HARMONY');
+  BSH_ADDRESS_MAP.set(process.env.ICON_NATIVE_COIN_NEAR_BSH_ADDRESS, 'NEAR');
 }
 
 function getBMCAddressesMap() {
