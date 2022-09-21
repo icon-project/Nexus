18fc51bea118d4e3dbc1fb4da4d8177baa2f6df6
Vọng Dương <vongduong@duongvongcse.local>
2022-09-21 10:58:52 +0700

    dashboard api - network - optimize get network list api
    get token prices only one time instead of each of every for loop

--- packages/dashboard-api/src/modules/networks/model.js
+++ packages/dashboard-api/src/modules/networks/model.js
@@ -12,16 +12,32 @@ const {
   getTokensbyNetworkId,
   getNetworkById: getNetworkByIdInDB,
   getTransactionsByNetworkIdAndTokenNames,
-  getPricesByTokenNames
+  getPricesByTokenNames,
 } = require('./repository');
 const { tokenToUsd, numberToFixedAmount } = require('../../common/util');
 
-async function getListNetworkConnectedIcon() {
+function setKeyValueTokenPrices(tokenPrices = []){
+  const tokenProperties = {};
+  tokenPrices.forEach(tokenPrice => {
+    tokenProperties[tokenPrice.name] = Number(tokenPrice.price);
+  });
+  return tokenProperties;
+}
 
-  const results = await Promise.allSettled([getNetworkInfo(), getTotalMintValue(), getTotalBurnValue(), getTokensVolume24h(), getTokenVolumeAllTime()]);
+async function getListNetworkConnectedIcon() {
+  const results = await Promise.allSettled([
+    getNetworkInfo(),
+    getTotalMintValue(),
+    getTotalBurnValue(),
+    getTokensVolume24h(),
+    getTokenVolumeAllTime(),
+  ]);
   for (let item of results) {
     if (item.status === 'rejected') {
-      logger.error('"getListNetworkConnectedIcon" failed while getting total transaction', item.reason);
+      logger.error(
+        '"getListNetworkConnectedIcon" failed while getting total transaction',
+        item.reason,
+      );
       throw new Error('"getListNetworkConnectedIcon" job failed: ' + item.reason);
     }
   }
@@ -32,22 +48,21 @@ async function getListNetworkConnectedIcon() {
   const tokensVolume24h = results[3].value;
   const tokensVolumeAllTime = results[4].value;
 
-  return await updateFiatVolume(
+  return await updateFiatVolumeV2(
     networks,
     tokensVolume24h,
     tokensVolumeAllTime,
     totalMintTokens,
     totalBurnTokens,
   );
-
 }
 
 async function updateFiatVolume(
-  networks,
-  tokensVolume24h,
-  tokensVolumeAllTime,
-  totalMintTokens,
-  totalBurnTokens,
+  networks = [],
+  tokensVolume24h = [],
+  tokensVolumeAllTime = [],
+  totalMintTokens = [],
+  totalBurnTokens = [],
 ) {
   for (let networkInfo of networks) {
     let USD24h = 0;
@@ -91,9 +106,61 @@ async function updateFiatVolume(
   return networks;
 }
 
+async function updateFiatVolumeV2(
+  networks = [],
+  tokensVolume24h = [],
+  tokensVolumeAllTime = [],
+  totalMintTokens = [],
+  totalBurnTokens = [],
+) {
+  let tokenNames = tokensVolume24h.concat(tokensVolumeAllTime).concat(totalMintTokens).concat(totalBurnTokens).map(item => item.tokenName);
+  tokenNames = [...new Set(tokenNames)];
+  const tokenPrices = await getPricesByTokenNames(tokenNames);
+  const tokenPriceProperties = setKeyValueTokenPrices(tokenPrices);
+  for (let networkInfo of networks) {
+    let USD24h = 0;
+    let USDAllTime = 0;
+    let USDMint = 0;
+    let USDBurn = 0;
+
+    for (const data of tokensVolume24h) {
+      if (data.networkId == networkInfo.id) {
+        const fiat = Number((Number(data.tokenVolume) * (tokenPriceProperties[data.tokenName] || 0)).toFixed(2));
+        USD24h += fiat;
+      }
+    }
+
+    for (const data of tokensVolumeAllTime) {
+      if (data.networkId == networkInfo.id) {
+        const fiat = Number((Number(data.tokenVolume) * (tokenPriceProperties[data.tokenName] || 0)).toFixed(2));
+        USDAllTime += fiat;
+      }
+    }
+
+    for (const data of totalMintTokens) {
+      if (data.networkId == networkInfo.id) {
+        const fiat = Number((Number(data.tokenValue) * (tokenPriceProperties[data.tokenName] || 0)).toFixed(2));
+        USDMint += fiat;
+      }
+    }
+
+    for (const data of totalBurnTokens) {
+      if (data.networkId == networkInfo.id) {
+        const fiat = Number((Number(data.tokenValue) * (tokenPriceProperties[data.tokenName] || 0)).toFixed(2));
+        USDBurn += fiat;
+      }
+    }
+
+    networkInfo.usd24h = numberToFixedAmount(USD24h);
+    networkInfo.usdAllTime = numberToFixedAmount(USDAllTime);
+    networkInfo.mintFee = USDMint;
+    networkInfo.burnFee = USDBurn;
+  }
+  return networks;
+}
 async function getNetworkById(networkId) {
   //	Get token_name from registered_tokens table related to networkId
-  const tokens = (await getTokensbyNetworkId(networkId)).map(element => element.token_name);
+  const tokens = (await getTokensbyNetworkId(networkId)).map((element) => element.token_name);
 
   if (tokens.length === 0) return null;
 
@@ -126,32 +193,29 @@ async function getNetworkById(networkId) {
   return result;
 }
 
-async function getNetworkByIdV2(networkId){
-  const tokens = (await getTokensbyNetworkId(networkId)).map(element => element.token_name);
-  
+async function getNetworkByIdV2(networkId) {
+  const tokens = (await getTokensbyNetworkId(networkId)).map((element) => element.token_name);
+
   const [transactions, prices] = await Promise.all([
     getTransactionsByNetworkIdAndTokenNames(networkId, tokens),
     getPricesByTokenNames(tokens),
   ]);
 
-  const priceProperty = {};
-  prices.forEach(price => {
-    priceProperty[price.name] = price.price;
-  });
+  const priceProperty = setKeyValueTokenPrices(prices);
 
   const at24hAgo = Date.now() - ONE_DAY_IN_MILLISECONDS;
   const result = [];
-  tokens.forEach(token => {
+  tokens.forEach((token) => {
     let volume24h = 0;
     let volume24hUSD = 0;
     let volumeAllTime = 0;
     let volumeAllTimeUSD = 0;
 
-    transactions.forEach(transaction => {
-      if(transaction.token_name !== token){
+    transactions.forEach((transaction) => {
+      if (transaction.token_name !== token) {
         return;
       }
-      if(Number(transaction.block_time) >= at24hAgo){
+      if (Number(transaction.block_time) >= at24hAgo) {
         volume24h += Number(transaction.value);
       }
       volumeAllTime += Number(transaction.value);
@@ -163,7 +227,7 @@ async function getNetworkByIdV2(networkId){
       volume24h: numberToFixedAmount(volume24h),
       volume24hUSD: numberToFixedAmount(volume24hUSD),
       volumeAllTime: numberToFixedAmount(volumeAllTime),
-      volumeAlTimeUSD: numberToFixedAmount(volumeAllTimeUSD)
+      volumeAlTimeUSD: numberToFixedAmount(volumeAllTimeUSD),
     });
   });
   return result;
@@ -175,5 +239,5 @@ module.exports = {
   getListNetworkConnectedIcon,
   getNetworkById,
   getNetworkByIdV2,
-  getNetworkByIdInDB
+  getNetworkByIdInDB,
 };
